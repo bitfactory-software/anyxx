d0ddcfd3d8eea52d357a07f526ad2424d5dce1d5
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-09-14 14:13:20 +0200

    add ref to v. romeo blog

--- ProxyWriteable/type_list.h
+++ ProxyWriteable/type_list.h
@@ -5,6 +5,7 @@ namespace BitFactory
     template< typename... ARGS >
     struct type_list
     {
+        // see https://vittorioromeo.info/index/blog/cpp20_lambdas_compiletime_for.html
         static void for_each( auto&& call )
         {
             ( call.template operator()< ARGS >(), ... );
