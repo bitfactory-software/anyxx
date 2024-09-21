19c98e8f070f4f9726a2dd94f8a0dde3aab19947
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-09-21 15:14:35 +0200

    complete proxy adapter

--- include/virtual_void/proxy/adapter.h
+++ include/virtual_void/proxy/adapter.h
@@ -37,14 +37,8 @@ namespace virtual_void::proxy
         ::add_facade< virtual_const_void_facade, true >
         ::add_convention< free_data, void*() >
         ::build {};
-    template< typename O > const void* data_( const O& o ) 
-    { 
-        return &o; 
-    }
-    template< typename O > void* data_( O& o ) 
-    { 
-        return &o; 
-    }
+    template< typename O > const void* data_( const O& o ) { return &o; }
+    template< typename O > void* data_( O& o ) { return &o; }
 
 
     PRO_DEF_FREE_DISPATCH( free_v_table_of, v_table_of_, v_table_of );
@@ -54,12 +48,40 @@ namespace virtual_void::proxy
         ::build {};
     template< typename O > v_table* v_table_of_( const O& o ) { return v_table_of< O >(); }
 
-    template< typename FACADE >
-    typed_const_void to_typed_const_void( const pro::proxy< FACADE >& p )
+    template< typename TYPED_VOID, typename FACADE >
+    auto to_typed_void_( const pro::proxy< FACADE >& p )
     {
         const std::type_info& type_info = pro::proxy_reflect< meta >( p ).type_info;
-        const void* cv = data( *p );
-        return { type_info, cv };
+        auto cv = data( *p );
+        return TYPED_VOID{ type_info, cv };
+    }
+    template< typename FACADE >
+    auto to_typed_const_void( const pro::proxy< FACADE >& p )
+    {
+        return to_typed_void_< typed_const_void >( p );
+    }
+    template< typename FACADE >
+    auto to_typed_void( const pro::proxy< FACADE >& p )
+    {
+        return to_typed_void_< typed_void >( p );
+    }
+
+    template< typename VIRTUAL_VOID, typename FACADE >
+    auto to_virtual_void_( const pro::proxy< FACADE >& p )
+    {
+        auto vt = v_table_of( *p );
+        auto cv = data( *p );
+        return VIRTUAL_VOID{ vt, cv };
+    }
+    template< typename FACADE >
+    auto to_virtual_const_void( const pro::proxy< FACADE >& p )
+    {
+        return to_virtual_void_< virtual_const_void >( p );
+    }
+    template< typename FACADE >
+    auto to_virtual_void( const pro::proxy< FACADE >& p )
+    {
+        return to_virtual_void_< typed_void >( p );
     }
 }
 
