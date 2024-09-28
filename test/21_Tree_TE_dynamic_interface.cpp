5f1729064d963a23c70db4d489a10c722632e5ff
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-09-28 17:48:43 +0200

    make dynamic_interface_traits.h, clean CMakeLists.txt, add 21_Tree_TE_dynamic_interface stump

---
+++ test/21_Tree_TE_dynamic_interface.cpp
@@ -0,0 +1,80 @@
+﻿//// virtual_void variant of this yomm2 example via c++RTTI
+//// https://github.com/jll63/yomm2/blob/master/examples/accept_no_visitors.cpp
+//
+//#include <iostream>
+//#include <iostream>
+//#include <string>
+//#include <memory>
+//
+//#include "include/catch.hpp"
+//
+//#include "../include/dynamic_interface/dynamic_interface.h"
+//#include "../include/virtual_void/virtual_void.h"
+//
+//using std::cout;
+//using std::string;
+//
+//namespace
+//{
+//
+//DECLARE_INTERFACE(node_i,
+//    (int, value),
+//    (string, as_forth),
+//    (string, as_lisp),
+//)
+//
+//using node = node_i< virtual_void::shared_const >;
+//
+//struct Plus  {
+//    Plus( node left, node right)
+//        : left(left), right(right) {
+//    }
+//    int value() const { return left.value() + right.value(); }
+//    string as_forth() const { return left->as_forth() + " " + right.as_forth() + " +"; }
+//    string as_lisp() const { return "(plus " + left.as_lisp() + " " + right.as_lisp() + ")"; }
+//
+//    node left, right;
+//};
+//
+//struct Times  {
+//    Times(node left, node right)
+//        : left(left), right(right) {
+//    }
+//    int value() const { return left.value() * right.value(); }
+//    string as_forth() const { return left.as_forth() + " " + right.as_forth() + " *"; }
+//    string as_lisp() const { return "(times " + left.as_lisp() + " " + right.as_lisp() + ")"; }
+//
+//    node left, right;
+//};
+//
+//struct Integer  {
+//    explicit Integer(int value) : int_(value) {
+//    }
+//    int value() const { return int_; }
+//    string as_forth() const { return std::to_string(int_); }
+//    string as_lisp() const { return std::to_string(int_); }
+//
+//    int int_;
+//};
+//
+//}
+//
+////TEST_CASE( "21_Tree_TE_dynamic_interface" ) 
+////{
+////    using virtual_void::make_shared_const;
+////
+////    auto expr = make_shared<Times>(
+////        make_shared<Integer>(2),
+////        make_shared<Plus>(make_shared<Integer>(3), make_shared<Integer>(4)));
+////
+////    REQUIRE( expr.value() == 14 );
+////    std::stringstream out;
+////    out << expr.as_forth() << " = " << expr.as_lisp() << " = " << expr.value();
+////    REQUIRE( out.str() == "2 3 4 + * = (times 2 (plus 3 4)) = 14" );
+////    std::cout << out.str() << "\n";
+////
+////    BENCHMARK("21_Tree_TE_dynamic_interface benchmark") {
+////        return expr.value();
+////    };
+////}
+////
--- test/dynamic_interface_test.cpp
+++ test/dynamic_interface_test.cpp
@@ -6,48 +6,9 @@
 #include "include/catch.hpp"
 
 #include "../include/dynamic_interface/dynamic_interface.h"
-#include "../include/virtual_void/virtual_void.h"
+#include "../include/virtual_void/dynamic_interface_traits.h"
 
 using namespace Catch::Matchers;
-
-namespace dynamic_interface
-{
-    template<>
-    struct trait< virtual_void::shared_const >
-    {
-        using type = virtual_void::shared_const;
-        
-        using param_t = type&;
-
-        template< typename FROM >
-        static type erase( FROM&& from )
-        {
-            using from_t = std::remove_cvref_t< FROM >;
-            if constexpr( std::is_base_of_v< type, from_t > )
-            {
-                return from;
-            }
-            else
-            {
-                return 	virtual_void::make_shared_const< from_t >( std::forward< FROM >( from ) );
-            }
-        }
-        template< typename CONSTRUCTOR_PARAM >
-        static auto unerase( type& from )
-        {
-            using constructor_param_t = std::remove_cvref_t< CONSTRUCTOR_PARAM >;
-            if constexpr( std::is_base_of_v< type, constructor_param_t > )
-            {
-                return static_cast< const constructor_param_t::type * >( from.data() );
-            }
-            else
-            {
-                return static_cast< const constructor_param_t* >( from.data() );
-            }
-        }
-    };
-
-};
 
 const double M_PI = 3.14;
 
