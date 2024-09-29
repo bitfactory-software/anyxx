c43a659189c2634c57d2ac095cdad599bc9b4ec0
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-09-29 10:20:46 +0200

    refactor index_table to smaler functions

--- include/perfect_typeid_hash/index_table.h
+++ include/perfect_typeid_hash/index_table.h
@@ -14,8 +14,10 @@
 namespace perfect_typeid_hash 
 {
 
-using type_id   = const std::type_info*;
-using index_t     = std::size_t;
+using type_id  = const std::type_info*;
+using index_t = std::size_t;
+
+const type_id unused_element = (type_id)-1;
 
 template< typename TARGET >
     requires ( std::convertible_to< TARGET, bool > )
@@ -28,6 +30,7 @@ struct index_table
     std::size_t hash_length = 0;
 
     std::vector<element_t> table;
+
 
     TARGET default_;
 
@@ -57,50 +60,51 @@ struct index_table
     {
         const auto element_count = elements.size();
 
-        std::default_random_engine rnd(13081963);
         std::size_t magnitude = 1;
 
         for (auto size = element_count * 5 / 4; size; size /= 2)
             ++magnitude;
-
-        std::uniform_int_distribution<std::uintptr_t> uniform_dist;
 
         for (std::size_t pass = 0; pass < 4; ++pass, ++magnitude) 
         {
             hash_shift = 8 * sizeof(type_id) - magnitude;
             table.resize( std::size_t(1) << magnitude );
             hash_length = 0;
-            std::size_t hash_max = 0;
 
-            const type_id unused_element = (type_id)-1;
-
-            bool found = false;
-            for(std::size_t attempts = 0; !found && attempts < 100000; ++attempts) 
-            {
-                std::fill(table.begin(), table.end(), std::pair{ unused_element, TARGET{} } );
-                
-                found = true;
-                hash_mult = uniform_dist(rnd) | 1;
-
-                for (const auto [ type, target ] : elements ) 
-                {
-                    auto index = apply_formula(type);
-                    hash_length = (std::max)(hash_length, index+1);
-
-                    if (found = ( table[index].first == unused_element))
-                        table[index] = std::pair{ type, target };
-                    else
-                        break;
-                }
-            }
-
-            if (!found)
-                continue;
-
-            return;
+            if( find_hash_mult_for_magnitude(elements) )
+                return;
         }
 
         throw "abort!";
+    }
+
+    bool find_hash_mult_for_magnitude( const std::vector< element_t >& elements )
+    {
+        std::default_random_engine random_engine(13081963);
+        std::uniform_int_distribution<std::uintptr_t> uniform_dist;
+        for(std::size_t attempts = 0; attempts < 100000; ++attempts) 
+        {
+            std::fill(table.begin(), table.end(), std::pair{ unused_element, TARGET{} } );                
+            hash_mult = uniform_dist(random_engine) | 1;
+            if( hash_mult_for_input_values( elements ) )
+                return true;
+        }
+        return false;
+    }
+
+    bool hash_mult_for_input_values( const std::vector< element_t >& elements )
+    {
+        for (const auto [ type, target ] : elements ) 
+        {
+            auto index = apply_formula(type);
+            hash_length = (std::max)(hash_length, index+1);
+
+            if( table[index].first == unused_element )
+                table[index] = std::pair{ type, target };
+            else
+                return false;
+        }
+        return true;
     }
 };
 
