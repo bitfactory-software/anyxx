dc6efc488c7bc0f30743a3ebb775d46e30923d3e
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-09-29 11:12:58 +0200

    add comment

--- include/perfect_typeid_hash/index_table.h
+++ include/perfect_typeid_hash/index_table.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// original algorithm from: https://github.com/jll63/yomm2 THANKS!
+// original algorithm from: https://github.com/jll63/yomm2 THANKS to Jean-Louis Leroy!
 // Copyright (c) 2018-2024 Jean-Louis Leroy
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt
