18c86511980b2c087201d3629acdecd9f3943c78
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-09-29 11:45:49 +0200

    refactor to any_dispatch/method

---
+++ include/any_dispatch/method.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <any>
+#include <functional>
+#include <map>
+#include <typeindex>
+
+namespace any_dispatch
+{
+    template< typename R, typename ANY, typename... ARGS >
+    class method
+    {
+		using erased_function_t = std::function< R( ANY, ARGS... ) >;
+	public:
+		std::map< std::type_index, erased_function_t > dispatchTable_;
+        erased_function_t fallback_ = []( ANY dispatch, ARGS... args )->R
+        {
+			throw std::runtime_error( std::string( dispatch.type().name() ) + ": No registered method." );
+        };
+	public:
+		void define_erased( const std::type_info& register_type_info, const erased_function_t& f )
+		{
+			auto entry = dispatchTable_.find( register_type_info );
+			if( entry != dispatchTable_.end() )
+				throw std::runtime_error( "Method for type already registered." );
+			dispatchTable_[ register_type_info ] = f;
+		}
+		auto define_fallback( const erased_function_t& fallback )
+		{
+            fallback_ = fallback;
+		}
+        template< typename DISPATCH >
+		auto define( const auto& f )
+		{
+			return define_erased( typeid( DISPATCH ), [ f ]( ANY dispatch, ARGS... args )->R
+			{
+                auto unerased = std::any_cast< DISPATCH >( &dispatch );   
+				return f( unerased, std::forward< ARGS >( args )... );
+			});
+		}
+		R operator()( ANY dispatch, ARGS... args ) const
+		{
+			auto entry = dispatchTable_.find( dispatch.type() );
+			if( entry == dispatchTable_.end() )
+				return fallback_( dispatch, args... );
+			return entry->second( dispatch, args... );
+		}	
+		erased_function_t is_defined( const std::type_info& type_info )
+		{
+			if( auto entry = dispatchTable_.find( type_info ); entry != dispatchTable_.end() )
+				return entry->second;
+			return nullptr_t{};
+		}
+		template< typename C >
+		erased_function_t is_defined()
+		{
+			return is_defined( typeid( C ) );
+		}
+    };
+}
+
---
+++ test/21_Tree_TE_dispatch_via_any.cpp
@@ -0,0 +1,132 @@
+﻿// virtual_void variant of this yomm2 example via virtual_void m_tables
+// https://github.com/jll63/yomm2/blob/master/examples/accept_no_visitors.cpp
+
+
+#include <iostream>
+#include <string>
+#include <any>
+
+#include "include/catch.hpp"
+
+#include "../../include/virtual_void/virtual_void.h"
+#include "../../include/virtual_void/utilities/unnamed__.h"
+
+using std::cout;
+using std::string;
+
+namespace
+{
+
+struct Node {
+};
+
+using any = std::any;
+
+struct Plus : Node {
+    Plus( any left, any right)
+        : left(left), right(right) {
+    }
+
+    any left, right;
+};
+
+struct Times : Node {
+    Times(any left, any right)
+        : left(left), right(right) {
+    }
+
+    any left, right;
+};
+
+struct Integer : Node {
+    explicit Integer(int value) : value(value) {
+    }
+//    ~Integer() { cout << "~Integer()" << "\n"; }
+};
+
+// =============================================================================
+// add behavior to existing classes, without changing them
+
+}
+
+
+namespace
+{
+// -----------------------------------------------------------------------------
+// evaluate
+
+auto value = virtual_void::method< int(const void*) >{ tree_domain };
+
+auto __ = value.override_< Plus >( []( auto expr ) {
+    return value( expr->left ) + value( expr->right );
+});
+
+auto __ = value.override_< Times >( []( auto expr ) {
+    return value( expr->left ) * value( expr->right );
+});
+
+auto __ = value.override_< Integer >( []( auto expr ) {
+    return expr->value;
+});
+
+// -----------------------------------------------------------------------------
+// render as Forth
+
+auto as_forth = virtual_void::method< string( const void* ) >{ tree_domain };
+
+auto __ = as_forth.override_< Plus >( []( auto expr ) {
+    return as_forth( expr->left ) + " " + as_forth( expr->right ) + " +";
+});
+
+auto __ = as_forth.override_< Times >( []( auto expr ) {
+    return as_forth( expr->left ) + " " + as_forth( expr->right ) + " *";
+});
+
+auto __ = as_forth.override_< Integer >( []( auto expr ) {
+    return std::to_string(expr->value);
+});
+
+// -----------------------------------------------------------------------------
+// render as Lisp
+
+auto as_lisp = virtual_void::method< string( const void* ) >{ tree_domain };
+
+auto __ = as_lisp.override_< Plus >( []( auto expr ) {
+    return "(plus " + as_lisp(expr->left) + " " + as_lisp(expr->right) + ")";
+});
+
+auto __ = as_lisp.override_< Times >( []( auto expr ) {
+    return "(times " + as_lisp(expr->left) + " " + as_lisp(expr->right) + ")";
+});
+
+auto __ = as_lisp.override_< Integer >( []( auto expr ) {
+    return std::to_string(expr->value);
+});
+
+}
+
+// -----------------------------------------------------------------------------
+
+TEST_CASE( "21_Tree_TE_dispach_via_m_table" )
+{
+    build_m_tables( tree_domain );
+
+    using virtual_void::make_shared_const;
+
+    auto expr = make_shared_const<Times>(
+        make_shared_const<Integer>(2),
+        make_shared_const<Plus>(make_shared_const<Integer>(3), make_shared_const<Integer>(4)));
+
+    REQUIRE( value(expr) == 14 );
+    std::stringstream out;
+    out << as_forth(expr) << " = " << as_lisp(expr) << " = " << value(expr);
+    REQUIRE( out.str() == "2 3 4 + * = (times 2 (plus 3 4)) = 14" );
+    std::cout << out.str() << "\n";
+
+    BENCHMARK("21_Tree_TE_dispach_via_m_table value") {
+        return value(expr);
+    };
+    BENCHMARK("21_Tree_TE_dispach_via_m_table as_lisp") {
+        return as_lisp(expr);
+    };
+}
