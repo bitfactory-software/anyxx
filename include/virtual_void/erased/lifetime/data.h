5a64c6a82b5f1b1e7a79008fdab1b9f8fab1e2cb
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-04 17:47:37 +0200

    abstract_data_t< MORE >

--- include/virtual_void/erased/lifetime/data.h
+++ include/virtual_void/erased/lifetime/data.h
@@ -8,27 +8,34 @@
 namespace virtual_void::erased
 {
 
-struct abstract_data
+struct empty_t {};
+
+template< typename MORE = empty_t >
+struct abstract_data_t : MORE
 {
 	void* data_ = nullptr;
-	abstract_data( void* data )
+	abstract_data_t( void* data )
 		: data_( data )
 	{}
 };
-template< typename M >
-struct concrete_data : abstract_data
+
+template< typename M, typename MORE = empty_t >
+struct concrete_data_t : abstract_data_t< MORE >
 {
 	M m_;
-	concrete_data( M&& m )
-		: abstract_data( std::addressof( m_ ) )
+	concrete_data_t( M&& m )
+		: abstract_data_t< MORE >( std::addressof( m_ ) )
 		, m_( std::move( m ) )
 	{}
 	template< typename... ARGS >
-	concrete_data( std::in_place_t,  ARGS&&... args )
-		: abstract_data( std::addressof( m_ ) )
+	concrete_data_t( std::in_place_t,  ARGS&&... args )
+		: abstract_data_t< MORE >( std::addressof( m_ ) )
 		, m_( std::forward< ARGS >( args )... )
 	{}
 };
+
+using abstract_data = abstract_data_t< empty_t >;
+template< typename M > using concrete_data = concrete_data_t< M, empty_t >;
 
 template< typename U > U* reconcrete_cast( abstract_data& a ) { return static_cast< U* >( a.data_ ); }
 template< typename U > const U* reconcrete_cast( const abstract_data& a ) { return static_cast< const U* >( a.data_ ); }
