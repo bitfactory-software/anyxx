586b335763553a52ffc892f25b6a61fd14c9a840
andreas pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-07 20:26:32 +0200

    TEST_CASE( "interface lifetime cast" ) fails .... ???

---
+++ include/virtual_void/erased/lifetime/cast.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <type_traits>
+#include <stdexcept>
+
+#include "observer.h"
+#include "shared_const.h"
+#include "unique.h"
+#include "value.h"
+
+namespace virtual_void::erased
+{
+	template< typename TO, typename FROM >  TO lifetime_cast( FROM const & );
+
+	template<>  const_observer lifetime_cast<const_observer >( shared_const const & shared_const )
+	{
+		return shared_const.data();
+	}
+	template<> const_observer lifetime_cast<const_observer >( unique const & unique )
+	{
+		return unique.data();
+	}
+	template<> const_observer lifetime_cast<const_observer >( value const & value )
+	{
+		return value.data();
+	}
+}
--- test/_erased_interface_lifetime_convert.cpp
+++ test/_erased_interface_lifetime_convert.cpp
@@ -6,43 +6,52 @@
 #include "include/catch.hpp"
 
 #include "../include/virtual_void/erased/interface.h"
-#include "../include/virtual_void/erased/lifetime/observer.h"
-#include "../include/virtual_void/erased/lifetime/shared_const.h"
+#include "../include/virtual_void/erased/lifetime/cast.h"
 
-using namespace::virtual_void;
+using namespace Catch::Matchers;
 
-ERASED_FREE_INTERFACE(to_string_i,
+using namespace virtual_void;
+
+const double M_PI = 3.14;
+
+struct position {float x, y;};
+
+struct X 
+{ 
+    std::string s_;
+    std::string to_string() const { return s_; } 
+};
+
+ERASED_INTERFACE( to_string_i,
     (std::string, to_string)
 )
 
-using to_string_vv = to_string_i< erased::shared_const >;
+using to_string_sc = to_string_i< erased::shared_const  >;
 using to_string_co = to_string_i< erased::const_observer >;
 
-std::string to_string_( auto const x )
+namespace virtual_void::erased
 {
-    return std::to_string( x.count_sides() );
+    template< typename ERASED_TO >
+    auto v_table_cast( const auto& v_table )
+    {
+        return reinterpret_cast< typename ERASED_TO::_v_table_t* >( v_table ); 
+    }
+
+    template< typename ERASED_TO, typename ERASED_FROM >
+    ERASED_TO interface_lifetime_cast( const ERASED_FROM& from )
+    {
+        return ERASED_TO
+            ( lifetime_cast< typename ERASED_TO::erased_t >( from._ref )
+            , v_table_cast< ERASED_TO >( from._v_table )
+            );
+    }
 }
 
-//namespace virtual_void::erased
-//{
-//    template< typename TO, typename ERASED_INTERFACE >
-//    TO interface_lifetime_cast( const ERASED_INTERFACE& interface )
-//    {
-//        auto casted = lifetime_cast< typename ERASED_INTERFACE::erased_t >( interface.get_erased() );
-//        return { casted, interface._v_table }; 
-//    }
-//
-//    //template< typename TO, typename ERASED_INTERFACE >
-//    //TO interface_lifetime_move_cast( )
-//    //{
-//    //
-//    //}
-//
-//}
-//
-//TEST_CASE( "erased interface change erased" ) 
-//{
-//    to_string_vv vv_{ 4711 };
-//    to_string_co co_{ vv_, erased::reconcrete_cast< int >( vv_.get_erased() ) }
-//
-//}
+TEST_CASE( "interface lifetime cast" ) 
+{
+    to_string_sc sc{ X{ "hallo" } };
+    REQUIRE( sc.to_string() == "hallo" );
+
+    to_string_co co = interface_lifetime_cast< to_string_co >( sc );
+    REQUIRE( co.to_string() == "hallo" );
+}
