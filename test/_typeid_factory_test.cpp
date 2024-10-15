caec419ae935499b1551d705e16ee81c6f5c61b7
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-15 20:47:29 +0200

    format

--- test/_typeid_factory_test.cpp
+++ test/_typeid_factory_test.cpp
@@ -2,87 +2,75 @@
 
 #include <any>
 
-#include "include/catch.hpp"
-
 #include "../include/virtual_void/m_table/lifetime/shared_const.h"
 #include "../include/virtual_void/m_table/lifetime/unique.h"
-#include "../include/virtual_void/typeid/factory.h"
 #include "../include/virtual_void/open_method/algorithm.h"
-
+#include "../include/virtual_void/typeid/factory.h"
 #include "class_hierarchy_test_hierarchy.h"
+#include "include/catch.hpp"
 
 using namespace virtual_void;
 
-namespace
-{
+namespace {
 
-TEST_CASE( "typeid factory" ) 
-{
-	using namespace TestDomain;
+TEST_CASE("typeid factory") {
+  using namespace TestDomain;
 
-	{
-		auto any_factory = typeid_::factory< std::any() >{};
-		using classes = type_list< D, C1, C2 >;
-		open_method::fill_with_overloads( classes{}, any_factory, []< typename T >()->std::any
-		{ 
-			//std::cout << "construct any for " << typeid( T ).name() << std::endl; 
-			return std::any( T() ); 
-		});
-		any_factory.seal_for_runtime();
-		auto test = [ & ]< typename T >()
-		{ 
-			auto a = any_factory( typeid( T ) );
-			REQUIRE( a.type() == typeid( T ) );
-		};
-		class_hierarchy::visit_classes< classes >( 
-			overload
-			{ [&]< typename C >				{ test.template operator()< C >(); }
-			, [&]< typename C, typename B >	{}
-			});
-	}
+  {
+    auto any_factory = typeid_::factory<std::any()>{};
+    using classes = type_list<D, C1, C2>;
+    open_method::fill_with_overloads(classes{}, any_factory,
+                                     []<typename T>() -> std::any {
+                                       // std::cout << "construct any for " <<
+                                       // typeid( T ).name() << std::endl;
+                                       return std::any(T());
+                                     });
+    any_factory.seal_for_runtime();
+    auto test = [&]<typename T>() {
+      auto a = any_factory(typeid(T));
+      REQUIRE(a.type() == typeid(T));
+    };
+    class_hierarchy::visit_classes<classes>(
+        overload{[&]<typename C> { test.template operator()<C>(); },
+                 [&]<typename C, typename B> {}});
+  }
 
-	{
-		auto factory = typeid_::factory< m_table::shared_const() >{};
-		using classes = type_list< D, C1, C2 >;
-		open_method::fill_with_overloads( classes{}, factory, []< typename T >()->m_table::shared_const
-		{ 
-			return m_table::make_shared_const< T >( typeid( T ).name() ); 
-		});
-		factory.seal_for_runtime();
-		auto test = [ & ]< typename T >()
-		{ 
-			auto cv = factory( typeid( T ) );
-			REQUIRE( cv.type() == typeid( T ) );
-			auto tp = static_cast< const T* >( cv.data() );
-			REQUIRE( tp->data == typeid( T ).name() );
-		};
-		class_hierarchy::visit_classes< classes >( 
-			overload
-			{ [&]< typename C >				{ test.template operator()< C >(); }
-			, [&]< typename C, typename B >	{}
-			});
-	}
-	{
-		auto factory = typeid_::factory< m_table::unique() >{};
-		using classes = type_list< D, C1, C2 >;
-		open_method::fill_with_overloads( classes{}, factory, []< typename T >()->m_table::unique
-		{ 
-			return m_table::make_unique< T >( typeid( T ).name() ); 
-		});
-		factory.seal_for_runtime();
-		auto test = [ & ]< typename T >()
-		{ 
-			auto cv = factory( typeid( T ) );
-			REQUIRE( cv.type() == typeid( T ) );
-			auto tp = static_cast< const T* >( cv.data() );
-			REQUIRE( tp->data == typeid( T ).name() );
-		};
-		class_hierarchy::visit_classes< classes >( 
-			overload
-			{ [&]< typename C >				{ test.template operator()< C >(); }
-			, [&]< typename C, typename B >	{}
-			});
-	}
+  {
+    auto factory = typeid_::factory<m_table::shared_const()>{};
+    using classes = type_list<D, C1, C2>;
+    open_method::fill_with_overloads(
+        classes{}, factory, []<typename T>() -> m_table::shared_const {
+          return m_table::make_shared_const<T>(typeid(T).name());
+        });
+    factory.seal_for_runtime();
+    auto test = [&]<typename T>() {
+      auto cv = factory(typeid(T));
+      REQUIRE(cv.type() == typeid(T));
+      auto tp = static_cast<const T*>(cv.data());
+      REQUIRE(tp->data == typeid(T).name());
+    };
+    class_hierarchy::visit_classes<classes>(
+        overload{[&]<typename C> { test.template operator()<C>(); },
+                 [&]<typename C, typename B> {}});
+  }
+  {
+    auto factory = typeid_::factory<m_table::unique()>{};
+    using classes = type_list<D, C1, C2>;
+    open_method::fill_with_overloads(
+        classes{}, factory, []<typename T>() -> m_table::unique {
+          return m_table::make_unique<T>(typeid(T).name());
+        });
+    factory.seal_for_runtime();
+    auto test = [&]<typename T>() {
+      auto cv = factory(typeid(T));
+      REQUIRE(cv.type() == typeid(T));
+      auto tp = static_cast<const T*>(cv.data());
+      REQUIRE(tp->data == typeid(T).name());
+    };
+    class_hierarchy::visit_classes<classes>(
+        overload{[&]<typename C> { test.template operator()<C>(); },
+                 [&]<typename C, typename B> {}});
+  }
 }
 
-}
+}  // namespace
