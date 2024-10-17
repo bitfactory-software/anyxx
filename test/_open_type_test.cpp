599385bdfe3d34e94d110e2b1bee7f6ffda00e20
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-17 16:02:07 +0200

    TEST_CASE("open object") { pass

--- test/_open_type_test.cpp
+++ test/_open_type_test.cpp
@@ -10,9 +10,25 @@ using namespace Catch::Matchers;
 
 using namespace virtual_void;
 
-struct test_object : open_object::members< test_object >{};
+struct test_object : open_object::members<test_object> {};
 
-struct test_member : open_object::member< test_object, test_member, std::string > {}; 
+struct test_member
+    : open_object::member<test_object, test_member, std::string> {};
+
+const open_object::member_table_index<test_object> open_object::member<test_object, test_member, std::string>::index;
 
 TEST_CASE("open object") {
+  REQUIRE(open_object::type_member_count_of<test_object>() == 1);
+  test_object a_test_object;
+  auto value = a_test_object.get(test_member());
+  REQUIRE(!value);
+  a_test_object.set(test_member(), std::string("hello world"));
+  auto r = a_test_object.get(test_member{});
+  REQUIRE(r);
+  std::cout << typeid(*r).name() << "\n";
+  //auto x = reconcrete_cast< std::string >(*r);
+  //std::cout << typeid(x).name() << "\n";
+  REQUIRE(*r == "hello world");
+  static_assert(std::same_as<decltype(r), std::optional<std::string>>);
+  // REQUIRE((*a_test_object.get(test_member())) == "hello world");
 }
