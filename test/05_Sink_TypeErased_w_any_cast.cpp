2e7d9883d7d73a3cf1293161006ec63e22b40b82
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-18 19:35:13 +0200

    05_Sink_TypeErased_w_any_cast

---
+++ test/05_Sink_TypeErased_w_any_cast.cpp
@@ -0,0 +1,66 @@
+﻿#include <any>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+#include <typeindex>
+#include <utility>
+#include <vector>
+
+#include "../../include/virtual_void/any_dispatch/method.h"
+#include "include/catch.hpp"
+
+namespace {
+
+namespace DB {
+using FactoryFunction = std::function<std::any(const std::string&)>;
+using SinkFunction = std::function<void(const std::any&)>;
+using Line = std::pair<std::string, std::string>;
+using File = std::vector<Line>;
+struct System {
+  File file;
+  std::map<std::string, FactoryFunction> factories;
+  void Query(std::string what, const SinkFunction& sink) {
+    for (auto [type, data] : file) sink(factories[type](data));
+  }
+};
+}  // namespace DB
+
+namespace Application {
+struct IntData {
+  int data = 0;
+};
+struct StringData {
+  std::string data;
+};
+struct DoubleData {
+  double data;
+};
+
+void ReportSink(const std::any& any) {
+  if (auto i = any_cast<IntData>(&any))
+    std::cout << "int: " << i->data << std::endl;
+  else if (auto s = any_cast<StringData>(&any))
+    std::cout << "string: " << s->data << std::endl;
+}
+
+void IntToOut(const IntData* i) {
+  std::cout << "int: " << i->data << std::endl;
+}
+
+}  // namespace Application
+
+TEST_CASE("05_Sink_TypeErased_w_any_cast") {
+  using namespace Application;
+  DB::System db;
+  db.factories["i"] = [](const std::string& data) -> std::any {
+    return IntData{std::atoi(data.c_str())};
+  };
+  db.factories["s"] = [](const std::string& data) -> std::any {
+    return StringData{data};
+  };
+  db.file = {{"i", "1"}, {"i", "4711"}, {"s", "hello"}, {"s", "world"}};
+  db.Query("junk", ReportSink);
+}
+
+}  // namespace
