6ffe56a212c6ffcc6f250573078b503eafd70018
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-19 10:50:36 +0200

    fix perfect_typeid_hash

--- include/virtual_void/perfect_typeid_hash/index_table.h
+++ include/virtual_void/perfect_typeid_hash/index_table.h
@@ -52,28 +52,31 @@ struct index_table {
   }
   void build(const auto& elements, TARGET default__) {
     default_ = default__;
-    if (auto found = find_hash(elements, inital_sparse_factor(elements.size())))
+    if (auto found = find_hash(elements, inital_sparse_base(elements.size())))
       hash_ = *found;
     else
       throw "abort!";
   }
-  auto static inital_sparse_factor(std::size_t element_count) {
-    std::size_t sparse_factor = 1;
-    for (auto size = element_count * 5 / 4; size; size /= 2) ++sparse_factor;
-    return sparse_factor;
+  auto static inital_sparse_base(std::size_t element_count) {
+    std::size_t sparse_base = 1;
+    for (auto size = element_count * 5 / 4; size >>= 1;) ++sparse_base;
+    return sparse_base;
   }
   static std::optional<hash_index> find_hash(const auto& elements,
-                                             std::size_t sparse_factor) {
-    for (std::size_t pass = 0; pass < 4; ++pass, ++sparse_factor)
-      if (auto found = find_hash_for_sparse_factor(elements, sparse_factor))
+                                             std::size_t sparse_base) {
+    for (std::size_t pass = 0; pass < 4; ++pass, ++sparse_base)
+      if (auto found = find_hash_for_sparse_base(elements, sparse_base))
         return found;
     return {};
   }
-  static std::optional<hash_index> find_hash_for_sparse_factor(
-      const auto& elements, std::size_t sparse_factor) {
+  static auto size_for_sparse_base(std::size_t sparse_base) {
+    return std::size_t(1) << sparse_base;
+  }
+  static std::optional<hash_index> find_hash_for_sparse_base(
+      const auto& elements, std::size_t sparse_base) {
     hash_index hash_index;
-    hash_index.shift = 8 * sizeof(type_id) - sparse_factor;
-    hash_index.table.resize(std::size_t(1) << sparse_factor);
+    hash_index.shift = 8 * sizeof(type_id) - sparse_base;
+    hash_index.table.resize(size_for_sparse_base(sparse_base));
     hash_index.length = 0;
 
     std::default_random_engine random_engine(13081963);
@@ -91,9 +94,10 @@ struct index_table {
     hash_index.mult = hash_mult;
     std::fill(hash_index.table.begin(), hash_index.table.end(),
               std::pair{unused_element, TARGET{}});
+    
     for (const auto& element : elements)
       if (!can_hash_input_value(element, hash_index)) return {};
-
+   
     return hash_index;
   }
   static std::optional<hash_index> can_hash_input_value(
@@ -101,7 +105,6 @@ struct index_table {
     auto index = hash_index.apply_formula(element.first);
     hash_index.length = (std::max)(hash_index.length, index + 1);
     if (hash_index.table[index].first != unused_element) return {};
-
     hash_index.table[index] = element;
     return hash_index;
   }
--- test/_typeid_hash_test.cpp
+++ test/_typeid_hash_test.cpp
@@ -1,23 +1,48 @@
+#include <cmath>
+#include <iostream>
 #include <string>
 #include <vector>
 
+#include "../include/virtual_void/perfect_typeid_hash/index_table.h"
 #include "include/catch.hpp"
 
-#include "../include/virtual_void/perfect_typeid_hash/index_table.h"
+TEST_CASE("build perfect typeid hash index") {
+  using entry_t = std::pair<perfect_typeid_hash::type_id, const char*>;
+  using table_t = std::vector<entry_t>;
+  table_t elements = {{&typeid(int), "int"},
+                      {&typeid(std::string), "std::string"},
+                      {&typeid(entry_t), "entry_t"}};
 
+  auto not_found = "not found";
+  perfect_typeid_hash::index_table<const char*> index_table(elements,
+                                                            not_found);
 
-TEST_CASE( "build perfect typeid hash index" ) {
+  REQUIRE(index_table[&typeid(float)] == not_found);
 
-	using entry_t = std::pair< perfect_typeid_hash::type_id, const char* >;
-	using table_t = std::vector<entry_t>;
-	table_t elements ={ { &typeid(int), "int" }, { &typeid(std::string), "std::string" }, { &typeid(entry_t), "entry_t" } }; 
-
-	auto not_found = "not found";
-	perfect_typeid_hash::index_table< const char* > index_table( elements, not_found );
-
-	REQUIRE( index_table[ &typeid(float) ] == not_found );
-
-	for( auto element : elements )
-		REQUIRE( index_table[ element.first ] == element.second );
+  for (auto element : elements)
+    REQUIRE(index_table[element.first] == element.second);
 }
 
+TEST_CASE("typeid perfect typeid hash index inital_sparse_base") {
+  std::cout << "typeid perfect typeid hash index inital_sparse_base"
+            << std::endl;
+  for (int i = 1; i <= 6; ++i) {
+    std::size_t size = (std::size_t)std::pow(10.0, i);
+    std::cout
+        << "| " << size << " | "
+        << perfect_typeid_hash::index_table<nullptr_t>::inital_sparse_base(size)
+        << " | "<< std::endl;
+    }
+}
+
+TEST_CASE("typeid perfect typeid hash index table.size()") {
+  std::cout << "typeid perfect typeid hash index table.size()"
+            << std::endl;
+  for (int sparse_base = 4; sparse_base <= 20; ++sparse_base) {
+    std::cout
+        << " | " << sparse_base << " | "
+        << perfect_typeid_hash::index_table<nullptr_t>::size_for_sparse_base(
+               sparse_base)
+        << " | "<< std::endl;
+  }
+}
