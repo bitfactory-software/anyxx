eb442995e4bad119ce9a49b6274dfe383d2a94bc
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-23 11:47:02 +0200

    lifetime_holder_ -> virtual_void_

--- include/virtual_void/erased/call_operator.h
+++ include/virtual_void/erased/call_operator.h
@@ -24,13 +24,11 @@ struct call_operator_v_table : BASE_V_TABLE {
   }
 };
 
-template <is_virtual_void VIRTUAL_VOID,
-          template <typename> typename BASE, typename CONST, typename RET,
-          typename... ARGS>
+template <is_virtual_void VIRTUAL_VOID, template <typename> typename BASE,
+          typename CONST, typename RET, typename... ARGS>
 struct call_operator_facade;
-template <is_virtual_void VIRTUAL_VOID,
-          template <typename> typename BASE, typename CONST, typename RET,
-          typename... ARGS>
+template <is_virtual_void VIRTUAL_VOID, template <typename> typename BASE,
+          typename CONST, typename RET, typename... ARGS>
 struct call_operator_facade<VIRTUAL_VOID, BASE, CONST, RET(ARGS...)>
     : BASE<VIRTUAL_VOID> {
  public:
@@ -40,8 +38,7 @@ struct call_operator_facade<VIRTUAL_VOID, BASE, CONST, RET(ARGS...)>
   using interface_base_t = base_t::v_table_t;
   using v_table_t = call_operator_v_table<interface_base_t, RET, ARGS...>;
   using query_interface_unique_t =
-      call_operator_v_table<virtual_void::erased::base<virtual_void_t>,
-                              void>;
+      call_operator_v_table<virtual_void::erased::base<virtual_void_t>, void>;
   template <typename T>
   using is_already_base =
       std::conditional_t<std::is_same_v<T, query_interface_unique_t>,
@@ -52,11 +49,11 @@ struct call_operator_facade<VIRTUAL_VOID, BASE, CONST, RET(ARGS...)>
 
  protected:
   using base_t::interface_impementation_;
-  using base_t::lifetime_holder_;
+  using base_t::virtual_void_;
 
  public:
-  call_operator_facade(virtual_void_t lifetime_holder, v_table_t* v_table)
-      : base_t(std::move(lifetime_holder), v_table) {}
+  call_operator_facade(virtual_void_t virtual_void, v_table_t* v_table)
+      : base_t(std::move(virtual_void), v_table) {}
   template <typename CONSTRUCTED_WITH>
   call_operator_facade(CONSTRUCTED_WITH&& v)
     requires(!std::derived_from<std::remove_cvref_t<CONSTRUCTED_WITH>, base_t>)
@@ -74,7 +71,7 @@ struct call_operator_facade<VIRTUAL_VOID, BASE, CONST, RET(ARGS...)>
              CONST, virtual_void_t>)
   {
     return static_cast<v_table_t*>(interface_impementation_)
-        ->call_op(base_t::lifetime_holder_.data(), std::forward<ARGS>(args)...);
+        ->call_op(base_t::virtual_void_.data(), std::forward<ARGS>(args)...);
   }
   call_operator_facade(const call_operator_facade&) = default;
   call_operator_facade(call_operator_facade&) = default;
@@ -85,11 +82,10 @@ struct call_operator_facade<VIRTUAL_VOID, BASE, CONST, RET(ARGS...)>
 };
 template <typename SIG, is_virtual_void VIRTUAL_VOID,
           template <typename> typename BASE = base>
-using call_operator =
-    call_operator_facade<VIRTUAL_VOID, BASE, const void, SIG>;
+using call_operator = call_operator_facade<VIRTUAL_VOID, BASE, const void, SIG>;
 
 template <typename SIG, is_virtual_void VIRTUAL_VOID,
-          template <typename> typename BASE = base >
+          template <typename> typename BASE = base>
 using mutable_call_operator =
     call_operator_facade<VIRTUAL_VOID, BASE, void, SIG>;
 
--- include/virtual_void/erased/interface.h
+++ include/virtual_void/erased/interface.h
@@ -39,18 +39,18 @@ class base {
   using is_already_base =
       std::false_type;  // base is always at the bottom of the interface chain.
  protected:
-  virtual_void_t lifetime_holder_ = nullptr;
+  virtual_void_t virtual_void_ = nullptr;
   v_table_t* interface_impementation_ = nullptr;
 
  public:
-  base(virtual_void_t lifetime_holder, v_table_t* v_table)
-      : lifetime_holder_(std::move(lifetime_holder)),
+  base(virtual_void_t virtual_void, v_table_t* v_table)
+      : virtual_void_(std::move(virtual_void)),
         interface_impementation_(v_table) {}
   template <typename CONSTRUCTED_WITH>
   base(CONSTRUCTED_WITH&& constructed_with)
     requires(!std::derived_from<std::remove_cvref_t<CONSTRUCTED_WITH>,
                                 base<VIRTUAL_VOID>>)
-      : lifetime_holder_(virtual_void::erased::erase_to<virtual_void_t>(
+      : virtual_void_(virtual_void::erased::erase_to<virtual_void_t>(
             std::forward<CONSTRUCTED_WITH>(constructed_with))) {
     static v_table_t imlpemented_interface{
         virtual_void::erased::unerase<VIRTUAL_VOID, CONSTRUCTED_WITH>()};
@@ -59,13 +59,13 @@ class base {
   template <typename OTHER>
   base(const OTHER& other)
     requires(std::derived_from<OTHER, base<VIRTUAL_VOID>>)
-      : lifetime_holder_(*other),
+      : virtual_void_(*other),
         interface_impementation_(other.get_interface()) {}
   base(const base&) = default;
   base(base&) = default;
   base(base&&) = default;
-  auto& operator*() const { return lifetime_holder_; }
-  auto& operator*() { return lifetime_holder_; }
+  auto& operator*() const { return virtual_void_; }
+  auto& operator*() { return virtual_void_; }
   v_table_t* get_interface() const { return interface_impementation_; }
   bool is_derived_from(const std::type_info& from) const {
     return interface_impementation_->_is_derived_from(from);
@@ -102,9 +102,8 @@ std::optional<TO> interface_cast(const FROM& from)
 
 template <typename TO, typename FROM>
 TO interface_lifetime_cast(const FROM& from) {
-  return TO(
-      lifetime_cast<typename TO::virtual_void_t>(*from),
-      v_table_cast<TO>(from.get_interface()));
+  return TO(lifetime_cast<typename TO::virtual_void_t>(*from),
+            v_table_cast<TO>(from.get_interface()));
 }
 
 }  // namespace virtual_void::erased
@@ -177,9 +176,10 @@ TO interface_lifetime_cast(const FROM& from) {
 #define _detail_INTERFACE_METHOD_H(l) _detail_INTERFACE_METHOD l
 #define _detail_LEAD_COMMA_H_E(l) _detail_LEAD_COMMA_H l
 
-#define _detail_INTERFACE_MAP_IMPL(type, name, const_, ...)                 \
-  auto name(T const_* x __VA_OPT__(, _detail_PARAM_LIST2(a, _sig, __VA_ARGS__))) { \
-    return x->name(__VA_OPT__(_detail_PARAM_LIST(a, _sig, __VA_ARGS__)));   \
+#define _detail_INTERFACE_MAP_IMPL(type, name, const_, ...)                  \
+  auto name(                                                                 \
+      T const_* x __VA_OPT__(, _detail_PARAM_LIST2(a, _sig, __VA_ARGS__))) { \
+    return x->name(__VA_OPT__(_detail_PARAM_LIST(a, _sig, __VA_ARGS__)));    \
   };
 
 #define _detail_INTERFACE_FUNCTION_PTR_DECL(type, name, const_, ...) \
@@ -195,10 +195,10 @@ TO interface_lifetime_cast(const FROM& from) {
 #define _detail_INTERFACE_METHOD(type, name, const_, ...)                   \
   type name(__VA_OPT__(_detail_PARAM_LIST2(a, _sig, __VA_ARGS__))) const_   \
     requires(virtual_void::erased::const_correct_for_lifetime_holder<       \
-             void const_, virtual_void_t>)                              \
+             void const_, virtual_void_t>)                                  \
   {                                                                         \
-    return static_cast<v_table_t*>(interface_impementation_)              \
-        ->name(base_t::lifetime_holder_.data()                              \
+    return static_cast<v_table_t*>(interface_impementation_)                \
+        ->name(base_t::virtual_void_.data()                                 \
                    __VA_OPT__(, _detail_PARAM_LIST(a, _sig, __VA_ARGS__))); \
   }
 
@@ -232,16 +232,16 @@ TO interface_lifetime_cast(const FROM& from) {
     };                                                                         \
   };                                                                           \
                                                                                \
-  template <virtual_void::erased::is_virtual_void VIRTUAL_VOID>   \
-  struct n : BASE<VIRTUAL_VOID> {                                           \
+  template <virtual_void::erased::is_virtual_void VIRTUAL_VOID>                \
+  struct n : BASE<VIRTUAL_VOID> {                                              \
    public:                                                                     \
-    using virtual_void_t = VIRTUAL_VOID;                                 \
-    using void_t = VIRTUAL_VOID::void_t;                                    \
-    using base_t = BASE<VIRTUAL_VOID>;                                      \
-    using interface_base_t = base_t::v_table_t;                              \
-    using v_table_t = n##interface<interface_base_t>;                        \
+    using virtual_void_t = VIRTUAL_VOID;                                       \
+    using void_t = VIRTUAL_VOID::void_t;                                       \
+    using base_t = BASE<VIRTUAL_VOID>;                                         \
+    using interface_base_t = base_t::v_table_t;                                \
+    using v_table_t = n##interface<interface_base_t>;                          \
     using query_interface_unique_t =                                           \
-        n##interface<virtual_void::erased::base<virtual_void_t>>;           \
+        n##interface<virtual_void::erased::base<virtual_void_t>>;              \
     template <typename T>                                                      \
     using is_already_base =                                                    \
         std::conditional_t<std::is_same_v<T, query_interface_unique_t>,        \
@@ -252,19 +252,19 @@ TO interface_lifetime_cast(const FROM& from) {
         "An interface my only be once in instanciated for a facade");          \
                                                                                \
    protected:                                                                  \
-    using base_t::lifetime_holder_;                                            \
+    using base_t::virtual_void_;                                               \
     using base_t::interface_impementation_;                                    \
                                                                                \
    public:                                                                     \
-    n(virtual_void_t lifetime_holder, v_table_t* v_table)                 \
-        : base_t(std::move(lifetime_holder), v_table) {}                       \
+    n(virtual_void_t virtual_void, v_table_t* v_table)                         \
+        : base_t(std::move(virtual_void), v_table) {}                          \
     template <typename CONSTRUCTED_WITH>                                       \
     n(CONSTRUCTED_WITH&& v)                                                    \
       requires(                                                                \
           !std::derived_from<std::remove_cvref_t<CONSTRUCTED_WITH>, base_t>)   \
         : base_t(std::forward<CONSTRUCTED_WITH>(v)) {                          \
-      static v_table_t imlpemented_interface{                                \
-          virtual_void::erased::unerase<VIRTUAL_VOID, CONSTRUCTED_WITH>()}; \
+      static v_table_t imlpemented_interface{                                  \
+          virtual_void::erased::unerase<VIRTUAL_VOID, CONSTRUCTED_WITH>()};    \
       interface_impementation_ = &imlpemented_interface;                       \
     }                                                                          \
     template <typename OTHER>                                                  \
@@ -286,4 +286,3 @@ TO interface_lifetime_cast(const FROM& from) {
   INTERFACE_METHOD_(ret, name, , __VA_ARGS__)
 #define INTERFACE_CONST_METHOD(ret, name, ...) \
   INTERFACE_METHOD_(ret, name, const, __VA_ARGS__)
-
