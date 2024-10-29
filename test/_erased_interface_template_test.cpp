6b7ea9498af51df1dc78ceef38db789be4c7b194
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-10-29 09:37:17 +0100

    cleanup _erased_interface_template_test

--- test/_erased_interface_template_test.cpp
+++ test/_erased_interface_template_test.cpp
@@ -3,19 +3,14 @@
 #include <map>
 #include <string>
 
-#include "../include/virtual_void/erased/data/has_no_meta/observer.h"
+//#include "../include/virtual_void/erased/data/has_no_meta/observer.h"
+#include "../include/virtual_void/erased/data/has_type_info/observer.h"
 #include "../include/virtual_void/erased/interface/declare_macro.h"
 #include "include/catch.hpp"
 
 using namespace Catch::Matchers;
 
 using namespace ::virtual_void::erased;
-
-const double M_PI = 3.14;
-
-struct position {
-  float x, y;
-};
 
 struct X {
   std::string s_;
@@ -35,7 +30,7 @@ TEST_CASE("erased template test") {
   std::map<std::string, int> map_string_to_int = {{"one", 1}, {"two", 2}};
 
   auto test_map_t_i =
-      [](map_t_i<data::has_no_meta::const_observer, std::string, int> map_i) {
+      [](map_t_i<data::has_type_info::const_observer, std::string, int> map_i) {
         REQUIRE(map_i.size() == 2);
         REQUIRE(map_i.at("one") == 1);
         REQUIRE(map_i.at("two") == 2);
