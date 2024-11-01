e4b24e812dced062ebf73d8e1f1e99f8579cc6dc
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-11-01 09:27:43 +0100

    decorated_ptr

--- include/virtual_void/data/decorated_ptr.h
+++ include/virtual_void/data/decorated_ptr.h
@@ -8,33 +8,33 @@
 namespace virtual_void::data {
 
 template <typename META, typename PTR>
-struct decorated_pointer : META {
+struct decorated_ptr : META {
   using meta_t = META;
  
-  decorated_pointer() = default;
-  decorated_pointer(const decorated_pointer&) = default;
-  decorated_pointer(decorated_pointer&) = default;
+  decorated_ptr() = default;
+  decorated_ptr(const decorated_ptr&) = default;
+  decorated_ptr(decorated_ptr&) = default;
   template <typename T>
-  decorated_pointer(T& v)
-    requires(!std::derived_from<T, decorated_pointer> &&
+  decorated_ptr(T& v)
+    requires(!std::derived_from<T, decorated_ptr> &&
              !std::same_as<std::decay_t<std::remove_pointer_t<T>>, void>)
       : META(std::in_place_type<T>), ptr_(&v) {}
   template <typename T>
-  decorated_pointer(const T& v)
-    requires(!std::derived_from<T, decorated_pointer> &&
+  decorated_ptr(const T& v)
+    requires(!std::derived_from<T, decorated_ptr> &&
              !std::same_as<std::decay_t<std::remove_pointer_t<T>>, void> &&
              is_const)
       : META(std::in_place_type<T>), ptr_(&v) {}
-  decorated_pointer(void_t v, const META& meta) : ptr_(v), META(meta) {}
+  decorated_ptr(void_t v, const META& meta) : ptr_(v), META(meta) {}
 
-  decorated_pointer(decorated_pointer&& rhs) noexcept { swap(*this, rhs); }
-  decorated_pointer& operator=(decorated_pointer&& rhs) noexcept {
-    decorated_pointer destroy_this{};
+  decorated_ptr(decorated_ptr&& rhs) noexcept { swap(*this, rhs); }
+  decorated_ptr& operator=(decorated_ptr&& rhs) noexcept {
+    decorated_ptr destroy_this{};
     swap(*this, destroy_this);
     swap(*this, rhs);
     return *this;
   }
-  friend void swap(decorated_pointer& lhs, decorated_pointer& rhs) noexcept {
+  friend void swap(decorated_ptr& lhs, decorated_ptr& rhs) noexcept {
     using namespace std;
     swap(lhs.ptr_, rhs.ptr_);
     swap(static_cast<META&>(lhs), static_cast<META&>(rhs));
