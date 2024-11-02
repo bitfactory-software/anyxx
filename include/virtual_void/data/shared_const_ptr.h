16d7b3846c5d791366b19cbb548ebcf3ed4e33d9
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-11-02 19:57:35 +0100

    clarify shared_const_ptr

--- include/virtual_void/data/shared_const_ptr.h
+++ include/virtual_void/data/shared_const_ptr.h
@@ -1,12 +1,11 @@
 #pragma once
 
-#include <memory>
-
+#include "erased_shared_const_ptr.h"
 #include "decorated_ptr.h"
 
 namespace virtual_void::data {
 
 template <typename META>
-using shared_const_ptr = decorated_ptr<std::shared_ptr<void const>, META>;
+using shared_const_ptr = decorated_ptr<erased_shared_const_ptr<void>, META>;
 
 }  // namespace virtual_void::data
