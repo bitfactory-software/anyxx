9de5d6ab1d52928073c734757da2c57a7f72afd9
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-11-02 19:57:59 +0100

    format

--- include/virtual_void/data/shared_const_ptr.h
+++ include/virtual_void/data/shared_const_ptr.h
@@ -4,8 +4,6 @@
 #include "decorated_ptr.h"
 
 namespace virtual_void::data {
-
 template <typename META>
 using shared_const_ptr = decorated_ptr<erased_shared_const_ptr<void>, META>;
-
 }  // namespace virtual_void::data
