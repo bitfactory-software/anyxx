c2d42367de4c9aef9e479f768f5d5a3ef9cd8e8b
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-11-03 09:17:47 +0100

    erased_std_ptr_trait

--- include/virtual_void/data/shared_const_ptr_trait.h
+++ include/virtual_void/data/shared_const_ptr_trait.h
@@ -1,23 +1,13 @@
 #pragma once
 
 #include "../virtual_void.h"
-#include "decorated_ptr_trait.h"
+#include "erased_std_ptr_trait.h"
 #include "shared_const_ptr.h"
 
 namespace virtual_void {
 
 template <typename META>
-struct shared_const_ptr_trait : decorated_ptr_trait<void const*, META> {
-  static void const* value(const auto& ptr) { return ptr.ptr_.get(); }
-
-  template <typename>
-  struct unerased_type_impl;
-  template <typename T>
-  struct unerased_type_impl<std::shared_ptr<T>> {
-    using value_type = T const;
-  };
-  template <typename CONSTRUCTED_WITH>
-  using unerased_type = unerased_type_impl<CONSTRUCTED_WITH>::value_type;
+struct shared_const_ptr_trait : erased_std_ptr_trait<std::shared_ptr, void const*, META> {
 
   template <typename V>
   static auto construct_from(std::shared_ptr<V> const& v) {
