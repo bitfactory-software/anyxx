f70e293c673c7bbbe41365b59857d553e61e67f4
Andi <andreas.pfaffenbichler@bitfactory.at>
2024-11-15 16:14:53 +0100

    query_interface works for const_observer

--- include/virtual_void/interface/registry.h
+++ include/virtual_void/interface/registry.h
@@ -16,47 +16,77 @@ using copy =
     virtual_void::open_method::via_type_info::factory<base<TO>(const FROM&)>;
 
 template <is_virtual_void TO, is_virtual_void FROM = TO>
+using copies =
+    virtual_void::open_method::via_type_info::factory<copy<TO, FROM> const&()>;
+
+template <is_virtual_void TO, is_virtual_void FROM = TO>
 using move =
     virtual_void::open_method::via_type_info::factory<base<TO>(FROM&&)>;
 
-template <typename META>
-struct factories {
-  copy<virtual_void::data::const_observer<META>> const_observer_copy;
-  copy<virtual_void::data::mutable_observer<META>> mutable_observer_copy;
-  copy<virtual_void::data::shared_const<META>> shared_const_copy;
-  // copy<virtual_void::data::unique<META>> unique_copy;
-  // copy<virtual_void::data::unique<META>,
-  //      virtual_void::data::shared_const<META>>
-  //     unique_copy_from_shared_const;
-
-  // move<virtual_void::data::shared_const<META>,
-  //      virtual_void::data::unique<META>>
-  //     move_to_shared_const;
-  // move<virtual_void::data::unique<META>,
-  //      virtual_void::data::unique<META>>
-  //     move_to_unique;
-};
-
 template <typename CLASS, typename META>
 copy<virtual_void::data::const_observer<META>> const_observer_copy;
+template <typename META>
+copies<virtual_void::data::const_observer<META>> const_observer_copies;
+
+template <typename CLASS, typename META>
+copy<virtual_void::data::mutable_observer<META>> mutable_observer_copy;
+template <typename CLASS, typename META>
+copy<virtual_void::data::shared_const<META>> shared_const_copy;
+// copy<virtual_void::data::unique<META>> unique_copy;
+// copy<virtual_void::data::unique<META>,
+//      virtual_void::data::shared_const<META>>
+//     unique_copy_from_shared_const;
+
+// move<virtual_void::data::shared_const<META>,
+//      virtual_void::data::unique<META>>
+//     move_to_shared_const;
+// move<virtual_void::data::unique<META>,
+//      virtual_void::data::unique<META>>
+//     move_to_unique;
 
 template <template <is_virtual_void> typename INTERFACE, typename CLASS,
           typename META>
-auto make_default_const_observer_copy() {
+auto default_const_observer_interface() {
   return +[](virtual_void::data::const_observer<META> const& from)
              -> base<virtual_void::data::const_observer<META>> {
     return INTERFACE<virtual_void::data::const_observer<META>>{
         *unchecked_unerase_cast<CLASS>(from)};
   };
 }
-template <template <is_virtual_void> typename INTERFACE, typename CLASS,
+template <template <is_virtual_void> typename TO_INTERFACE, typename CLASS,
           typename META>
-void enable_const_observer_copy(
-    copy<virtual_void::data::const_observer<META>>& const_observer_copy) {
-  const_observer_copy
-      .define<INTERFACE<virtual_void::data::const_observer<META>>>(
-          make_default_const_observer_copy<INTERFACE, CLASS, META>());
+void enable_const_observer_copy() {
+  using target_interface_t =
+      TO_INTERFACE<virtual_void::data::const_observer<META>>;
+  const_observer_copy<CLASS, META>.define<target_interface_t>(
+      default_const_observer_interface<TO_INTERFACE, CLASS, META>());
+  if (!const_observer_copies<META>.is_defined<CLASS>())
+    const_observer_copies<META>.define<CLASS>(
+        +[]() -> copy<virtual_void::data::const_observer<META>> const& {
+          return const_observer_copy<CLASS, META>;
+        });
 };
+
+template <is_virtual_void TO, is_virtual_void FROM>
+const auto& copy_factory(const std::type_info& type_info) {
+  using meta_t = typename virtual_void_trait<TO>::meta_t;
+  using from_meta_t = virtual_void_trait<FROM>::meta_t;
+  static_assert(std::is_same_v<meta_t, from_meta_t>);
+  if constexpr (std::same_as<TO, FROM>) {
+    if constexpr (std::same_as<virtual_void::data::const_observer<meta_t>,
+                               TO>) {
+      return const_observer_copies<meta_t>(type_info);
+    } else if constexpr (std::same_as<
+                             virtual_void::data::mutable_observer<meta_t>,
+                             TO>) {
+    } else if constexpr (std::same_as<virtual_void::data::shared_const<meta_t>,
+                                      TO>) {
+    }
+  } else {
+    static_assert(false);
+    return {};
+  }
+}
 
 // template <template< is_virtual_void > class INTERFACE, is_virtual_void VV>
 // struct query_interface_;
@@ -64,5 +94,20 @@ void enable_const_observer_copy(
 // struct query_interface_<INTERFACE<VV>> {
 //
 // };
+
+// template<typename
+
+template <typename TO_INTERFACE, typename FROM_INTERFACE>
+FROM_INTERFACE query_interface(const FROM_INTERFACE& from_interface) {
+  using vv_to_t = typename TO_INTERFACE::virtual_void_t;
+  using vv_from_t = typename FROM_INTERFACE::virtual_void_t;
+  static_assert(is_virtual_void<vv_to_t>);
+  static_assert(is_virtual_void<vv_from_t>);
+  const auto& vv_from = get_virtual_void(from_interface);
+  auto const& type_info = *get_meta(vv_from)->type_info();
+  auto const& copy = copy_factory<vv_to_t, vv_from_t>(type_info);
+  auto base = copy.construct<TO_INTERFACE>(vv_from);
+  return static_v_table_cast<FROM_INTERFACE>(base);
+}
 
 };  // namespace virtual_void::interface
--- test/_prototype.cpp
+++ test/_prototype.cpp
@@ -2,12 +2,11 @@
 
 #include "include/catch.hpp"
 
-//import virtual_void;
-#include "virtual_void/interface/declare_macro.h"
-
+// import virtual_void;
 #include "virtual_void/data/has_type_info/observer.h"
 #include "virtual_void/data/has_type_info/shared_const.h"
 #include "virtual_void/interface/base.h"
+#include "virtual_void/interface/declare_macro.h"
 #include "virtual_void/interface/registry.h"
 
 using namespace virtual_void;
@@ -29,8 +28,10 @@ ERASED_INTERFACE(set_value_i, (INTERFACE_METHOD(void, set_value, double)))
 }  // namespace
 
 TEST_CASE("prototype") {
-  enable_const_observer_copy<get_value_i, X, meta>(const_observer_copy<X, meta>);
+  enable_const_observer_copy<get_value_i, X, meta>();
+//      const_observer_copy<X, meta>);
   const_observer_copy<X, meta>.seal_for_runtime();
+  const_observer_copies<meta>.seal_for_runtime();
   //  get_value_i<const_observer> get_value_i_co =
   //  query_interface<get_value_i<const_observer>>(registry);
 
@@ -45,8 +46,12 @@ TEST_CASE("prototype") {
   auto base =
       const_observer_copy<X, meta>.construct<get_value_i<const_observer>>(
           get_virtual_void(to_string_i_co));
-  auto i =
-      interface::static_v_table_cast<get_value_i<const_observer>>(base);
+  auto i = interface::static_v_table_cast<get_value_i<const_observer>>(base);
   REQUIRE(i.get_value() == 3.14);
   std::cout << "prototype: " << i.get_value() << "\n";
+
+  get_value_i<const_observer> i1 =
+      query_interface<get_value_i<const_observer>>(to_string_i_co);
+  REQUIRE(i1.get_value() == 3.14);
+  std::cout << "prototype i1: " << i1.get_value() << "\n";
 }
