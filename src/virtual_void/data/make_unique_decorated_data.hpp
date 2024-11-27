560453f6246ab62e641e81701c075c591b1b21d5
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-11-27 14:42:36 +0100

    simplify

--- src/virtual_void/data/make_unique_decorated_data.hpp
+++ src/virtual_void/data/make_unique_decorated_data.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "erased_unique.hpp"
+#include "erased_unique_ptr.hpp"
 
 namespace virtual_void::data {
 
 template <typename T, typename... ARGS>
 auto make_unique_decorated_data(ARGS&&... args) {
-  return make_erased_unique<typename T::base_t, T>(std::in_place,
-                                          std::forward<ARGS>(args)...);
+  auto deleter = +[](typename T::base_t* meta) { delete static_cast<T*>(meta); };
+  return erased_unique_ptr<typename T::base_t>(new T(std::in_place, std::forward<ARGS>(args)...), deleter);
 }
 
 }  // namespace virtual_void::data
--- src/virtual_void/data/unique.hpp
+++ src/virtual_void/data/unique.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "erased_unique.hpp"
+#include "erased_unique_ptr.hpp"
 #include "decorated_data.hpp"
 
 namespace virtual_void::data {
 template <typename META>
-using unique = erased_unique<decoration_base<META>>;
+using unique = erased_unique_ptr<decoration_base<META>>;
 }  // namespace virtual_void::data
