a96ad5d56d037a7a5b1365070e3cda35df8243b3
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-12-19 21:02:44 +0100

    table.hpp

--- virtual_void/meta/i_table.hpp
+++ virtual_void/meta/i_table.hpp
@@ -3,39 +3,11 @@
 #include <virtual_void/data/has_i_table/unique.hpp>
 #include <virtual_void/interface/base.hpp>
 #include <virtual_void/meta/archetype.hpp>
+#include <virtual_void/meta/table.hpp>
 
 namespace virtual_void::meta {
 
 VV_EXPORT int next_i_table_index_value();
-
-template <typename TRAGET, auto DEFAULT>
-class table {
-  std::vector<TRAGET> table_;
-  constexpr void ensure_size(std::size_t v_table_index) {
-    if (table_.size() <= v_table_index)
-      table_.insert(table_.end(), 1 + v_table_index - table_.size(), DEFAULT);
-  }
-
- public:
-  constexpr table() = default;
-  constexpr table(table const&) = delete;
-  constexpr void register_target(int index, TRAGET target) {
-    ensure_size(index);
-    table_[index] = target;
-  }
-  constexpr void clear() { table_.clear(); }
-
-  constexpr TRAGET at(int v_table_index) const {
-    if (table_.size() <= v_table_index) return DEFAULT;
-    auto target = table_[v_table_index];
-    return target;
-  }
-  constexpr TRAGET find(int v_table_index) const {
-    if (table_.size() > v_table_index) return table_[v_table_index];
-    return DEFAULT;
-  }
-  constexpr auto size() const { return table_.size(); }
-};
 
 class index_table_for_v_table_in_i_table {
   table<int, -1> table_;
