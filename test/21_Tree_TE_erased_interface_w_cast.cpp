ae857d1485a615f913c89d2f578a6b5bd27ae11e
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-12-20 20:03:06 +0100

    add benchmark for value vial attach_interface

--- test/21_Tree_TE_erased_interface_w_cast.cpp
+++ test/21_Tree_TE_erased_interface_w_cast.cpp
@@ -30,12 +30,16 @@ using node = node_i<shared_const>;
 
 namespace _21_Tree_TE_erased_interface_w_cast {
 ERASED_INTERFACE(lisp_i, (INTERFACE_CONST_METHOD(string, as_lisp)))
-}
+ERASED_INTERFACE(value2_i, (INTERFACE_CONST_METHOD(int, value2)))
+}  // namespace _21_Tree_TE_erased_interface_w_cast
 
 namespace _21_Tree_TE_erased_interface_w_cast {
 
 auto as_lisp_(auto const& v) {
   return attach_interface<lisp_i<const_observer>>(v).as_lisp();
+}
+auto value2_(auto const& v) {
+  return attach_interface<value2_i<const_observer>>(v).value2();
 }
 
 struct Plus {
@@ -47,6 +51,7 @@ struct Plus {
   string as_lisp() const {
     return "(plus " + as_lisp_(left) + " " + as_lisp_(right) + ")";
   }
+  int value2() const { return value2_(left) + value2_(right); }
 
   node left, right;
 };
@@ -60,6 +65,7 @@ struct Times {
   string as_lisp() const {
     return "(times " + as_lisp_(left) + " " + as_lisp_(right) + ")";
   }
+  int value2() const { return value2_(left) * value2_(right); }
 
   node left, right;
 };
@@ -69,6 +75,7 @@ struct Integer {
   int value() const { return int_; }
   string as_forth() const { return std::to_string(int_); }
   string as_lisp() const { return std::to_string(int_); }
+  int value2() const { return int_; }
 
   int int_;
 };
@@ -84,6 +91,7 @@ using namespace _21_Tree_TE_erased_interface_w_cast;
 
 VV_DECLARE_V_TABLE_INDEX(, _21_Tree_TE_erased_interface_w_cast::node_i)
 VV_DECLARE_V_TABLE_INDEX(, _21_Tree_TE_erased_interface_w_cast::lisp_i)
+VV_DECLARE_V_TABLE_INDEX(, _21_Tree_TE_erased_interface_w_cast::value2_i)
 
 template <>
 struct i_table_of<Plus> : i_table_implementation_of<Plus> {};
@@ -96,11 +104,15 @@ is_a<Plus, lisp_i_v_table> __;
 is_a<Times, lisp_i_v_table> __;
 is_a<Integer, lisp_i_v_table> __;
 
+is_a<Plus, value2_i_v_table> __;
+is_a<Times, value2_i_v_table> __;
+is_a<Integer, value2_i_v_table> __;
+
 TEST_CASE("21_Tree_TE_erased_interface_w_cast") {
   using namespace virtual_void;
   using namespace _21_Tree_TE_erased_interface_w_cast;
 
-  REQUIRE(get_i_table_of<Times>().size() == 1u);
+  REQUIRE(get_i_table_of<Times>().size() == 2u);
   auto expr = node(make_node<Times>(
       make_node<Integer>(2),
       make_node<Plus>(make_node<Integer>(3), make_node<Integer>(4))));
@@ -113,9 +125,11 @@ TEST_CASE("21_Tree_TE_erased_interface_w_cast") {
   std::cout << out.str() << "\n";
 #ifndef _DEBUG
   BENCHMARK("21_Tree_TE_dynamic_interface value") { return expr.value(); };
+  BENCHMARK("21_Tree_TE_dynamic_interface value2") { return value2_(expr); };
   BENCHMARK("21_Tree_TE_dynamic_interface as_lisp") { return as_lisp_(expr); };
 #endif  // !_DEBUG
 }
 
 VV_DEFINE_V_TABLE_INDEX(_21_Tree_TE_erased_interface_w_cast ::node_i)
 VV_DEFINE_V_TABLE_INDEX(_21_Tree_TE_erased_interface_w_cast ::lisp_i)
+VV_DEFINE_V_TABLE_INDEX(_21_Tree_TE_erased_interface_w_cast ::value2_i)
