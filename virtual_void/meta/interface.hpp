3019453ebf75694b208808df324114c177ff0fcf
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-09-23 21:17:15 +0200

    cleanup meta::interface

--- virtual_void/meta/interface.hpp
+++ virtual_void/meta/interface.hpp
@@ -17,12 +17,6 @@ class interface {
   template <typename T>
   constexpr interface(std::in_place_type_t<T>) {}
 
-  int register_archetype(archetype& archetype) {
-    return index_(archetype, &archetype::interface_count_);
-  }
-  int register_archetype_at(archetype& archetype, int index) {
-    return index_.register_at(archetype, index);
-  }
 };
 
 }  // namespace virtual_void::meta
