2490c0cb8a9624efeabe01b9befaafadb6e5dd9a
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-09-23 21:33:40 +0200

    delete archetype.*

--- virtual_void/meta/archetype.cpp
+++
@@ -1,5 +0,0 @@
-#include <virtual_void/meta/archetype.hpp>
-
-using namespace virtual_void;
-using namespace virtual_void::meta;
-
--- virtual_void/meta/class.hpp
+++ virtual_void/meta/class.hpp
@@ -4,7 +4,6 @@
 #include <typeindex>
 #include <vector>
 #include <virtual_void/data/has_meta_runtime/unique.hpp>
-#include <virtual_void/meta/archetype.hpp>
 #include <virtual_void/meta/class.hpp>
 #include <virtual_void/meta/table.hpp>
 #include <virtual_void/utillities/overload.hpp>
