cab14eb6b1640fa10b2b41d8d86b2dceb8b4315f
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-09-27 18:10:29 +0200

    drop decoration from shared_const(_trait)

--- virtual_void/data/shared_const.hpp
+++ virtual_void/data/shared_const.hpp
@@ -7,7 +7,7 @@
 #include <virtual_void/data/shared_const_trait.hpp>
 
 namespace virtual_void::data {
-using shared_const = std::shared_ptr<decoration_base const>;
+using shared_const = std::shared_ptr<void const>;
 }  // namespace virtual_void::data
 
 namespace virtual_void {
--- virtual_void/data/shared_const_trait.hpp
+++ virtual_void/data/shared_const_trait.hpp
@@ -1,30 +1,28 @@
 #pragma once
 
-#include <virtual_void/virtual_void.hpp>
-#include <virtual_void/data/decorated_data.hpp>
 #include <virtual_void/data/make_shared_const_decorated_data.hpp>
+#include <virtual_void/virtual_void.hpp>
 
 namespace virtual_void {
 
 struct shared_const_trait : virtual_void_default_unerase {
-  
   using void_t = void const*;
   template <typename V>
-  using typed_t = data::decorated_data<V>;
+  using typed_t = const std::decay_t<V>;
   static constexpr bool is_constructibile_from_const = true;
 
-  static void const* value(const auto& ptr) { return ptr->value(); }
+  static void const* value(const auto& ptr) { return ptr.get(); }
   static auto meta(const auto& ptr) { return ptr->get_meta(); }
   static bool has_value(const auto& ptr) { return static_cast<bool>(ptr); }
 
   template <typename V>
   static auto construct_from(V&& v) {
-    return data::make_shared_const_decorated_data<typed_t<std::decay_t<V>>>(
-        std::forward<V>(v));
+    return construct_in_place(std::in_place_type<V>, std::forward<V>(v));
   }
   template <typename V, typename... ARGS>
   static auto construct_in_place(std::in_place_type_t<V>, ARGS&&... args) {
-    return data::make_shared_const_decorated_data<typed_t<V>>(std::forward<ARGS>(args)...);
+    return static_pointer_cast<void const>(
+        std::make_shared<typed_t<V>>(std::forward<ARGS>(args)...));
   }
 };
 
