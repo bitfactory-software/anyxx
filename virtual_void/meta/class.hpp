ca321a74bf17520d01d35bd15b985934a5ddef9c
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-09-27 10:30:53 +0200

    drop class_with_bases

--- virtual_void/meta/class.hpp
+++ virtual_void/meta/class.hpp
@@ -91,56 +91,7 @@ constexpr void visit_classes(auto visitor) {
       [&]<typename CLASS>() { visit_class<CLASS, deep>(visitor); });
 }
 
-using bases_t = std::vector<const std::type_info*>;
-struct class_with_bases {
-  const std::type_info* self;
-  bases_t bases;
-};
-using classes_with_bases = std::map<std::type_index, class_with_bases>;
 
-constexpr auto fill_runtime_registry(classes_with_bases& registry) {
-  return overload{
-      [&]<typename C> { registry[typeid_of<C>()].self = &typeid_of<C>(); },
-      [&]<typename C, typename B> {
-        registry[typeid_of<C>()].bases.emplace_back(&typeid_of<B>());
-      }};
-}
-template <typename CLASS, bool deep = true>
-constexpr nullptr_t declare(classes_with_bases& registry) {
-  meta::visit_class<CLASS, deep>(fill_runtime_registry(registry));
-  return {};
-}
-template <typename CLASSES, bool deep = true>
-constexpr nullptr_t declare_classes(classes_with_bases& registry) {
-  meta::visit_classes<CLASSES, deep>(fill_runtime_registry(registry));
-  return {};
-}
-template <typename CLASS>
-constexpr auto declare_deep(classes_with_bases& registry) {
-  return declare<CLASS, true>(registry);
-}
-template <typename CLASS>
-constexpr auto declare_shallow(classes_with_bases& registry) {
-  return declare<CLASS, false>(registry);
-}
-
-constexpr void visit_hierarchy(const std::type_index& self,
-                               const classes_with_bases& classes_with_bases,
-                               auto visitor);
-inline constexpr void visit_bases(const bases_t& bases,
-                                  const classes_with_bases& classes_with_bases,
-                                  auto visitor) {
-  for (auto base : bases) visit_hierarchy(*base, classes_with_bases, visitor);
-}
-inline constexpr void visit_hierarchy(
-    const std::type_index& self, const classes_with_bases& classes_with_bases,
-    auto visitor) {
-  auto found = classes_with_bases.find(self);
-  if (found == classes_with_bases.end())
-    throw std::runtime_error("class not registered.");
-  visitor(*found->second.self);
-  visit_bases(found->second.bases, classes_with_bases, visitor);
-}
 
 template <typename CLASS, typename V_TABLE>
 struct is_a {
