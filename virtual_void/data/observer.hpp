6d513872cf5e72ac577da48d4453b1b684f317a8
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-09-28 14:00:41 +0200

    delete decorated_ptr*.hpp

--- virtual_void/data/observer.hpp
+++ virtual_void/data/observer.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
-#include <virtual_void/data/decorated_ptr.hpp>
 #include <virtual_void/data/observer_trait.hpp>
 
 namespace virtual_void::data {
 
 template <typename VV_VOID>
-using observer = decorated_ptr<VV_VOID>;
+using observer = VV_VOID;
 
 using const_observer = observer<void const*>;
 using mutable_observer = observer<void*>;
@@ -14,9 +13,12 @@ using mutable_observer = observer<void*>;
 }
 
 namespace virtual_void {
-template <typename VV_VOID>
-struct erased_data_trait<data::observer<VV_VOID>>
-    : observer_trait<data::observer<VV_VOID>, VV_VOID> {};
+template <>
+struct erased_data_trait<data::const_observer>
+    : observer_trait<data::const_observer, data::const_observer> {};
+template <>
+struct erased_data_trait<data::mutable_observer>
+    : observer_trait<data::mutable_observer, data::mutable_observer> {};
 }  // namespace virtual_void
 
 namespace virtual_void::data::has_no_meta {
--- virtual_void/data/observer_trait.hpp
+++ virtual_void/data/observer_trait.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
-#include <virtual_void/data/decorated_ptr_trait.hpp>
+#include <virtual_void/virtual_void.hpp>
 
 namespace virtual_void {
 
 template <typename ERASED_DATA, typename VV_VOID>
-struct observer_trait : decorated_ptr_trait<VV_VOID> {
-  using base_trait = decorated_ptr_trait<VV_VOID>;
-  static constexpr bool is_constructibile_from_const = base_trait::is_const;
+struct observer_trait : virtual_void_default_unerase {
+  using void_t = VV_VOID;
+  static constexpr bool is_const = is_const_void<void_t>;
+  static constexpr bool is_constructibile_from_const = is_const;
 
-  static VV_VOID value(const auto& ptr) { return ptr.ptr_; }
+  static bool has_value(const auto& ptr) { return static_cast<bool>(ptr); }
+  static VV_VOID value(const auto& ptr) { return ptr; }
 
   template <typename V>
   static auto construct_from(V& v) {
@@ -18,7 +20,7 @@ struct observer_trait : decorated_ptr_trait<VV_VOID> {
   }
   template <typename V>
   static auto construct_from(const V& v)
-    requires(base_trait::is_const)
+    requires(is_const)
   {
     return ERASED_DATA(static_cast<VV_VOID>(&v));
   }
@@ -29,7 +31,7 @@ struct observer_trait : decorated_ptr_trait<VV_VOID> {
   }
   template <typename V>
   static auto construct_in_place(std::in_place_type_t<V>, const V& arg)
-    requires base_trait::is_const
+    requires is_const
   {
     return construct_from(arg);
   }
