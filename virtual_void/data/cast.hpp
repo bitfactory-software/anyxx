382d6eafa5146ac2f63e86414fc7370f58b82634
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-01 20:20:43 +0200

    rename

--- virtual_void/data/cast.hpp
+++ virtual_void/data/cast.hpp
@@ -7,36 +7,36 @@
 namespace virtual_void::data {
 
 template <is_erased_data TO, is_erased_data FROM>
-struct cast_converter;
+struct cast_trait;
 
 template <typename TO, typename FROM>
 concept castable_from =
     is_erased_data<FROM> && is_erased_data<TO> && requires(FROM f) {
-      { cast_converter<TO, FROM>{}(f) } -> std::same_as<TO>;
+      { cast_trait<TO, FROM>{}(f) } -> std::same_as<TO>;
     };
 
 template <is_erased_data FROM>
   requires(!is_const_data<FROM>)
-struct cast_converter<mutable_observer, FROM> {
+struct cast_trait<mutable_observer, FROM> {
   auto operator()(const auto& from) {
     return mutable_observer{get_void_data_ptr(from)};
   }
 };
 template <is_erased_data FROM>
-struct cast_converter<const_observer, FROM> {
+struct cast_trait<const_observer, FROM> {
   auto operator()(const auto& from) {
     return const_observer{get_void_data_ptr(from)};
   }
 };
 template <>
-struct cast_converter<shared_const, shared_const> {
+struct cast_trait<shared_const, shared_const> {
   auto operator()(const auto& from) { return from; }
 };
 
 template <typename TO, typename FROM>
   requires castable_from<TO, FROM>
 TO cast_to(FROM const& from) {
-  return cast_converter<TO, FROM>{}(from);
+  return cast_trait<TO, FROM>{}(from);
 }
 
 static_assert(!castable_from<mutable_observer, const_observer>);
