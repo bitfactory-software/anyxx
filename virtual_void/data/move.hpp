d96487c21f90b3bb06ff96dd32d4be2609bc9fa7
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-02 10:15:42 +0200

    moveable_from, can_move_to_from

--- virtual_void/data/move.hpp
+++ virtual_void/data/move.hpp
@@ -1,39 +1,52 @@
 #pragma once
 
 #include <virtual_void/data/observer.hpp>
+#include <virtual_void/data/shared_const.hpp>
+#include <virtual_void/data/unique.hpp>
 
 namespace virtual_void::data {
 
 template <is_erased_data TO, is_erased_data FROM>
-struct move_converter;
+bool constexpr can_move_to_from = false;
 
 template <typename TO, typename FROM>
 concept moveable_from =
-    is_erased_data<FROM> && is_erased_data<TO> && requires(FROM&& f) {
-      { move_converter<TO, FROM>{}(std::move(f)) } -> std::same_as<TO>;
-    };
+    is_erased_data<FROM> && is_erased_data<TO> && can_move_to_from<TO, FROM>;
 
-template <is_erased_data TO, is_erased_data FROM>
-struct move_converter {
-  TO operator()(FROM&& from) { return std::move(from); }
-};
+template <is_erased_data X>
+bool constexpr can_move_to_from<X, X> = true;
 
-template <voidness VOIDNESS, is_erased_data FROM>
-  requires(!voidness<FROM>)
-struct move_converter<observer<VOIDNESS>, FROM> {
-  auto operator()(auto&&) { static_assert(false, "no move to observer!"); }
-};
+template <>
+bool constexpr can_move_to_from<shared_const, unique> = true;
 
 template <voidness TO, voidness FROM>
   requires const_correct_call<is_const_void<TO>, is_const_void<FROM>>
-struct move_converter<observer<TO>, FROM> {
-  TO operator()(auto&& from) { return from; }
-};
+bool constexpr can_move_to_from<TO, FROM> = true;
 
 template <typename TO, typename FROM>
   requires moveable_from<TO, std::decay_t<FROM>>
 TO move_to(FROM&& from) {
-  return move_converter<TO, FROM>{}(std::move(from));
+  return std::move(from);
 }
+
+static_assert(!moveable_from<mutable_observer, const_observer>);
+static_assert(moveable_from<mutable_observer, mutable_observer>);
+static_assert(!moveable_from<mutable_observer, unique>);
+static_assert(!moveable_from<mutable_observer, shared_const>);
+
+static_assert(moveable_from<const_observer, const_observer>);
+static_assert(moveable_from<const_observer, mutable_observer>);
+static_assert(!moveable_from<const_observer, unique>);
+static_assert(!moveable_from<const_observer, shared_const>);
+
+static_assert(!moveable_from<shared_const, const_observer>);
+static_assert(!moveable_from<shared_const, mutable_observer>);
+static_assert(moveable_from<shared_const, unique>);
+static_assert(moveable_from<shared_const, shared_const>);
+
+static_assert(!moveable_from<unique, const_observer>);
+static_assert(!moveable_from<unique, mutable_observer>);
+static_assert(moveable_from<unique, unique>);
+static_assert(!moveable_from<unique, shared_const>);
 
 };  // namespace virtual_void::data
