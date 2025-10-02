f3dd3cfe9d19f684552f5c648c8ad5f1d5bd1ea7
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-02 10:52:28 +0200

    drop dead code

--- virtual_void/interface/clone.hpp
+++ virtual_void/interface/clone.hpp
@@ -17,11 +17,6 @@ std::expected<TO_INTERFACE, virtual_void::runtime::cast_error> clone_to(
   });
 }
 
-template <is_interface TO_INTERFACE, data::is_erased_data VV_FROM>
-auto clone_to(VV_FROM const& vv_from, runtime::base_v_table const* from) {
-  return borrow_as<TO_INTERFACE>(vv_from, *from->meta_data);
-}
-
 template <is_interface TO_INTERFACE, is_interface FROM_INTERFACE>
 auto clone_to(const FROM_INTERFACE& from_interface) {
   return clone_to<TO_INTERFACE>(get_erased_data(from_interface),
