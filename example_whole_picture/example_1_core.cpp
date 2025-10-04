b698c5d85f4aeac057e8b99de6247e807e8048a2
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-04 16:49:41 +0200

    refactor to picture

--- example_whole_picture/example_1_core.cpp
+++ example_whole_picture/example_1_core.cpp
@@ -22,20 +22,23 @@ TEST_CASE("example 1 core") {
 
 namespace {
 
-struct buffer {
-  std::vector<std::vector<char> > buffer_;
-  buffer(std::size_t size_x, std::size_t size_y) {
-    for (std::size_t line : std::views::iota(0, (int)size_y))
-      buffer_.emplace_back(std::vector<char>(size_x, ' '));
-  }
+class picture {
+  using line = std::vector<char>;
+  std::vector<line> lines_;
+
+  static auto make_line(std::size_t size_x) { return line(size_x, ' '); }
+
+ public:
+  picture(std::size_t size_x, std::size_t size_y)
+      : lines_(size_y, make_line(size_x)) {}
   void write(point p, char ch) {
-    if (p.x >= 0 && p.x < buffer_[0].size())
-      if (p.y >= 0 && p.y < buffer_.size()) {
-        buffer_[p.y][p.x] = ch;
+    if (p.x >= 0 && p.x < lines_[0].size())
+      if (p.y >= 0 && p.y < lines_.size()) {
+        lines_[p.y][p.x] = ch;
       }
   }
   void flush() const {
-    for (const auto& line : buffer_) {
+    for (const auto& line : lines_) {
       for (const auto& ch : line) std::cout << ch;
       std::cout << "\n";
     }
@@ -44,12 +47,12 @@ struct buffer {
 
 }  // namespace
 
-VV_RUNTIME_STATIC(buffer)
-runtime::class_<buffer>::implements<surface> __;
+VV_RUNTIME_STATIC(picture)
+runtime::class_<picture>::implements<surface> __;
 
 TEST_CASE("example 2 core circle") {
   for (auto r : std::views::iota(1, 7)) {
-    buffer b{80, 25};
+    picture b{80, 25};
     auto c = make_circle({12, 12}, r);
     mutable_observed_surface s{b};
     c.draw(s);
@@ -59,28 +62,28 @@ TEST_CASE("example 2 core circle") {
 
 TEST_CASE("example 3 core line") {
   {
-    buffer b{80, 25};
+    picture b{80, 25};
     auto l = make_line({0, 25}, {80, 0});
     mutable_observed_surface s{b};
     l.draw(s);
     b.flush();
   }
   {
-    buffer b{80, 25};
+    picture b{80, 25};
     auto l = make_line({0, 0}, {0, 25});
     mutable_observed_surface s{b};
     l.draw(s);
     b.flush();
   }
   {
-    buffer b{80, 25};
+    picture b{80, 25};
     auto l = make_line({0, 0}, {80, 0});
     mutable_observed_surface s{b};
     l.draw(s);
     b.flush();
   }
   {
-    buffer b{80, 25};
+    picture b{80, 25};
     auto l = make_line({0, 0}, {80, 25});
     mutable_observed_surface s{b};
     l.draw(s);
