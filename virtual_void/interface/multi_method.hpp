ad87f778f8664b7f0964d020d0349f1b7c22f1f0
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-09 19:52:52 +0200

    remove construcion rests

--- virtual_void/interface/multi_method.hpp
+++ virtual_void/interface/multi_method.hpp
@@ -2,137 +2,6 @@
 
 #include <virtual_void/interface/extension_method.hpp>
 
-#pragma once
-
-#include <virtual_void/interface/extension_method.hpp>
-
-//namespace virtual_void::interface {
-//
-//template <is_interface FIRST, is_interface SECOND, typename R, typename... ARGS>
-//class multi_method;
-//
-//template <is_interface FIRST, is_interface SECOND, typename R, typename... ARGS>
-//  requires has_extension_methods_enabled<FIRST> &&
-//           has_extension_methods_enabled<SECOND>
-//class multi_method<FIRST, SECOND, R(ARGS...)> {
-// public:
-//  using interface1_t = FIRST;
-//  using v_table1_t = extended_v_table<FIRST>;
-//  using interface2_t = FIRST;
-//  using v_table2_t = extended_v_table<SECOND>;
-//
-//  using CONSTNESS1 = typename nth_t<0, ARGS...>;
-//  using VOID1 = void_t<CONSTNESS1>;
-//  using observer_interface1_t = interface1_t::template type_for<VOID1>;
-//
-//  using CONSTNESS2 = typename nth_t<1, ARGS...>;
-//  using VOID2 = void_t<CONSTNESS2>;
-//  using observer_interface2_t = interface2_t::template type_for<VOID2>;
-//
-//  template <typename CLASS>
-//  using class1_param_t = self_pointer<VOID1>::template type<CLASS>;
-//  template <typename CLASS>
-//  using class2_param_t = self_pointer<VOID2>::template type<CLASS>;
-//
-//  using erased_function_t =
-//      typename translate_erased_function<R, ARGS...>::type;
-//  //
-//  //  template <typename... ARGS>
-//  //  struct default_function_impl;
-//  //  template <is_constness CONSTNESS, typename... OTHER_ARGS>
-//  //  struct default_function_impl<CONSTNESS, OTHER_ARGS...> {
-//  //    using type = R (*)(observer_interface_t const&, OTHER_ARGS...);
-//  //    static R noop(observer_interface_t const&, OTHER_ARGS...) {
-//  //      if constexpr (std::same_as<R, void>) {
-//  //        return;
-//  //      } else {
-//  //        return {};
-//  //      }
-//  //    }
-//  //  };
-//  //  using default_function = default_function_impl<ARGS...>;
-//  //
-// private:
-//  std::size_t index1_ = extension_method_count_of<v_table1_t> ++;
-//  std::size_t index2_ = extension_method_count_of<v_table2_t> ++;
-//  std::vector<std::vector<erased_function_t>> dispatch_matrix_;
-//  std::size_t dispatch_dimension_size_1_ = 0;
-//  std::size_t dispatch_dimension_size_2_ = 0;
-//
-//  ////    default_function::type default_ = default_function::noop;
-//  //
-// public:
-//  multi_method(multi_method const&) = delete;
-//  // multi_method(default_function::type f = default_function::noop)
-//  //     : default_(f) {}
-//  multi_method() = default;
-//
-//  template <typename... OTHER_ARGS>
-//  auto operator()(observer_interface1_t const& model1,
-//                  observer_interface2_t const& model2,
-//                  OTHER_ARGS&&... args) const {
-//    auto v_table1 = get_v_table(model1)->extension_method_table;
-//    auto dispatch_dim1 = runtime::get_multi_method_index_at(v_table1, index1_);
-//
-//    auto v_table2 = get_v_table(model2)->extension_method_table;
-//    auto dispatch_dim2 = runtime::get_multi_method_index_at(v_table2, index2_);
-//
-//    assert(dispatch_dim1 && dispatch_dim2);
-//
-//    auto target = dispatch_matrix_[*dispatch_dim1][*dispatch_dim2];
-//    assert(target);
-//
-//    auto erased_function = reinterpret_cast<erased_function_t>(target);
-//    return (erased_function)(get_void_data_ptr(model1),
-//                             get_void_data_ptr(model2),
-//                             std::forward<OTHER_ARGS>(args)...);
-//  }
-//  //  template <typename CLASS, typename... OTHER_ARGS>
-//  //  auto operator()(CLASS const* p, OTHER_ARGS&&... args) const {
-//  //    return (*this)(observer_interface_t{*p},
-//  //    std::forward<OTHER_ARGS>(args)...);
-//  //  }
-//  template <typename CLASS1, typename CLASS2, typename FUNCTION>
-//  auto define(FUNCTION f) {
-//    auto fp = ensure_function_ptr2<CLASS1, class1_param_t, CLASS2,
-//                                   class2_param_t, R, ARGS...>(f);
-//
-//    auto v_table1 =
-//        runtime::extension_method_table_instance<v_table1_t, CLASS1>();
-//    auto dispatch_dim1 =
-//        *runtime::get_multi_method_index_at(v_table1, index1_)
-//             .or_else([&] -> std::optional<std::size_t> {
-//               runtime::set_multi_method_index_at(v_table1, index1_,
-//                                                  dispatch_dimension_size_1_);
-//               return dispatch_dimension_size_1_++;
-//             });
-//
-//    auto v_table2 =
-//        runtime::extension_method_table_instance<v_table2_t, CLASS2>();
-//    auto dispatch_dim2 =
-//        *runtime::get_multi_method_index_at(v_table2, index2_)
-//             .or_else([&] -> std::optional<std::size_t> {
-//               runtime::set_multi_method_index_at(v_table2, index2_,
-//                                                  dispatch_dimension_size_2_);
-//               return dispatch_dimension_size_2_++;
-//             });
-//
-//    if (dispatch_matrix_.size() <= dispatch_dim1)
-//      dispatch_matrix_.resize(dispatch_dim1 + 1);
-//    if (dispatch_matrix_[dispatch_dim1].size() <= dispatch_dim2)
-//      dispatch_matrix_[dispatch_dim1].resize(dispatch_dim2 + 1);
-//
-//    dispatch_matrix_[dispatch_dim1][dispatch_dim2] =
-//        reinterpret_cast<erased_function_t>(fp);
-//
-//    return fp;
-//  }
-//};
-//
-//}  // namespace virtual_void::interface
-
-////---------------------------------------------------------
-
 namespace virtual_void::interface {
 
 template <is_interface INTERFACE>
@@ -199,10 +68,11 @@ struct args_to_tuple {
 template <is_interface INTERFACE, typename... METHOD_ARGS>
 struct args_to_tuple<virtual_<INTERFACE>, METHOD_ARGS...> {
   template <typename T, typename ACTUAL_ARG, typename... ACTUAL_ARGS>
-  auto operator()(T && dispatch_args, ACTUAL_ARG&& dispatch_arg,
+  auto operator()(T&& dispatch_args, ACTUAL_ARG&& dispatch_arg,
                   ACTUAL_ARGS&&... args) {
-    return args_to_tuple<METHOD_ARGS...>{}(std::tuple_cat(
-        std::forward<T>(dispatch_args), std::make_tuple(get_void_data_ptr(dispatch_arg))),
+    return args_to_tuple<METHOD_ARGS...>{}(
+        std::tuple_cat(std::forward<T>(dispatch_args),
+                       std::make_tuple(get_void_data_ptr(dispatch_arg))),
         std::forward<ACTUAL_ARGS>(args)...);
   }
 };
@@ -213,7 +83,7 @@ struct dispatch_matrix {
 };
 template <typename DISPATCH, is_interface INTERFACE, typename... ARGS>
 struct dispatch_matrix<DISPATCH, virtual_<INTERFACE>, ARGS...> {
-    using type = typename dispatch_matrix<std::vector<DISPATCH>, ARGS...>::type;
+  using type = typename dispatch_matrix<std::vector<DISPATCH>, ARGS...>::type;
 };
 
 template <typename R, typename... ARGS>
@@ -238,7 +108,8 @@ struct multi_method<R(ARGS...)> {
       return fp;
     }
     template <typename DISPATCH_MATRIX, typename ARGS_TUPLE>
-    auto invoke(DISPATCH_MATRIX const& target, ARGS_TUPLE&& args, auto&&...) const {
+    auto invoke(DISPATCH_MATRIX const& target, ARGS_TUPLE&& args,
+                auto&&...) const {
       return std::apply(target, std::forward<ARGS_TUPLE>(args));
     }
   };
@@ -268,9 +139,11 @@ struct multi_method<R(ARGS...)> {
       return next_t::template define<CLASSES...>(fp, matrix[dispatch_index]);
     }
 
-    template <typename DISPATCH_MATRIX, typename ARGS_TUPLE, typename... ACTUAL_ARGS>
+    template <typename DISPATCH_MATRIX, typename ARGS_TUPLE,
+              typename... ACTUAL_ARGS>
     auto invoke(DISPATCH_MATRIX const& target, ARGS_TUPLE&& args_tuple,
-                INTERFACE const& interface, ACTUAL_ARGS&&... actual_args) const {
+                INTERFACE const& interface,
+                ACTUAL_ARGS&&... actual_args) const {
       auto extension_method_table =
           get_v_table(interface)->extension_method_table;
       auto dispatch_dim =
@@ -286,14 +159,15 @@ struct multi_method<R(ARGS...)> {
 
   template <typename... CLASSES>
   auto define(auto f) {
-    auto fp = ensure_function_ptr_from_functor_t<R, CLASSES...>::template instance<ARGS...>(f);
+    auto fp = ensure_function_ptr_from_functor_t<
+        R, CLASSES...>::template instance<ARGS...>(f);
     return dispatch_access_.template define<CLASSES...>(fp, dispatch_matrix_);
   };
 
   template <typename... ACTUAL_ARGS>
   auto operator()(ACTUAL_ARGS&&... args) {
-    auto args_tuple = args_to_tuple<ARGS...>{}(std::tuple<>{},
-                                            std::forward<ACTUAL_ARGS>(args)...);
+    auto args_tuple = args_to_tuple<ARGS...>{}(
+        std::tuple<>{}, std::forward<ACTUAL_ARGS>(args)...);
     return dispatch_access_.invoke(dispatch_matrix_, args_tuple,
                                    std::forward<ACTUAL_ARGS>(args)...);
   }
