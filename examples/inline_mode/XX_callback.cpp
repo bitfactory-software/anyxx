e5a478a298dc7c6a9a0e2a0e600dd410dd28e14d
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-21 17:15:47 +0200

    reorg tests

--- examples/inline_mode/XX_callback.cpp
+++ examples/inline_mode/XX_callback.cpp
@@ -124,18 +124,29 @@ auto wrap(api<R> api) {
 TEST_CASE("example XX/ recieve1") {
   int step = 1;
 
-  auto int_recieve_api = [&]() {
-    return coro_callback::wrap<int>([&](std::function<void(int)> recieve) {
+ auto int_callback_api = ([&](std::function<void(int)> recieve) {
       std::println("before recieve");
       CHECK(step++ == 1);
       recieve(42);
       std::println("after recieve");
       CHECK(step++ == 3);
     });
-  };
 
+  step = 1;
+  CHECK(step == 1);
+  int_callback_api([&](int _42) {
+    std::println("recieving 42");
+    CHECK(step++ == 2);
+    CHECK(42 == _42);
+  });
+  CHECK(step == 4);
+
+  auto int_recieve_coro = [&]() {
+    return coro_callback::wrap<int>(int_callback_api);
+  };
+  step = 1;
   auto test1 = [&] -> coro_callback::recieve<int> {
-    auto _42 = co_await int_recieve_api();
+    auto _42 = co_await int_recieve_coro();
     std::println("recieving 42");
     CHECK(step++ == 2);
     CHECK(42 == _42);
@@ -149,22 +160,32 @@ TEST_CASE("example XX/ recieve1") {
 TEST_CASE("example XX/ recieve2") {
   int step = 1;
 
-  auto void_recieve_api = [&]() {
-    return coro_callback::wrap<void>([&](std::function<void(void)> recieve) {
-      std::println("before recieve");
-      CHECK(step++ == 1);
-      recieve();
-      std::println("after recieve");
-      CHECK(step++ == 3);
-    });
+  auto void_callback_api = [&](std::function<void(void)> recieve) {
+    std::println("before recieve");
+    CHECK(step++ == 1);
+    recieve();
+    std::println("after recieve");
+    CHECK(step++ == 3);
   };
 
-  auto test2 = [&] -> coro_callback::recieve<void> {
-    co_await void_recieve_api();
+  step = 1;
+  CHECK(step == 1);
+  void_callback_api([&] {
+    std::println("recieving");
+    CHECK(step++ == 2);
+  });
+  CHECK(step == 4);
+
+  auto void_recieve_coro = [&]() {
+    return coro_callback::wrap<void>(void_callback_api);
+  };
+  step = 1;
+  auto test2coro = [&] -> coro_callback::recieve<void> {
+    co_await void_recieve_coro();
     std::println("recieving");
     CHECK(step++ == 2);
   };
   CHECK(step == 1);
-  test2();
+  test2coro();
   CHECK(step == 4);
 }
