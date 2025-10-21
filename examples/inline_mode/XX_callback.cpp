f42b68bc9accaa5fefc725cbd434d2f766870c2a
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-21 20:36:19 +0200

    clarify tests for coro_callback

--- examples/inline_mode/XX_callback.cpp
+++ examples/inline_mode/XX_callback.cpp
@@ -9,6 +9,7 @@ using namespace Catch::Matchers;
 TEST_CASE("example XX/ recieve1") {
   int step = 1;
 
+  // + lib callback style
   auto int_callback_api = ([&](std::function<void(int)> recieve) {
     std::println("before recieve");
     CHECK(step++ == 1);
@@ -16,7 +17,9 @@ TEST_CASE("example XX/ recieve1") {
     std::println("after recieve");
     CHECK(step++ == 3);
   });
+  // - lib callback style
 
+  // + app callback style
   step = 1;
   CHECK(step == 1);
   int_callback_api([&](int _42) {
@@ -25,26 +28,32 @@ TEST_CASE("example XX/ recieve1") {
     CHECK(42 == _42);
   });
   CHECK(step == 4);
+  // - app callback style
 
+  // + lib wrapped for coro style
   auto int_recieve_coro = [&]() {
     return coro_callback::wrap<int>(int_callback_api);
   };
+  // - lib wrapped for coro style
   step = 1;
   auto test1 = [&] -> coro_callback::recieve<int> {
+    // + app coro style must exist inside acoro
     auto _42 = co_await int_recieve_coro();
     std::println("recieving 42");
     CHECK(step++ == 2);
     CHECK(42 == _42);
     co_return _42;
+    // - app coro style
   };
   CHECK(step == 1);
-  CHECK(test1().get_result() == 42);
+  CHECK(test1().get_result() == 42);  // leak result out of coro callback
   CHECK(step == 4);
 }
 
 TEST_CASE("example XX/ recieve2") {
   int step = 1;
 
+  // + lib callback style
   auto void_callback_api = [&](std::function<void(void)> recieve) {
     std::println("before recieve");
     CHECK(step++ == 1);
@@ -52,7 +61,9 @@ TEST_CASE("example XX/ recieve2") {
     std::println("after recieve");
     CHECK(step++ == 3);
   };
+  // - lib callback style
 
+  // + app callback style
   step = 1;
   CHECK(step == 1);
   void_callback_api([&] {
@@ -60,15 +71,20 @@ TEST_CASE("example XX/ recieve2") {
     CHECK(step++ == 2);
   });
   CHECK(step == 4);
+  // - app callback style
 
+  // + lib wrapped for coro style
   auto void_recieve_coro = [&]() {
     return coro_callback::wrap<void>(void_callback_api);
   };
+  // - lib wrapped for coro style
   step = 1;
   auto test2coro = [&] -> coro_callback::recieve<void> {
+    // + app coro style must exist inside acoro
     co_await void_recieve_coro();
     std::println("recieving");
     CHECK(step++ == 2);
+    // - app coro style
   };
   CHECK(step == 1);
   test2coro();
