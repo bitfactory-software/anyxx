5207eb22b8ae359a30949cf93289499e465aa368
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-12-01 20:03:18 +0100

    wip

--- examples/inline_mode/_2_stringable_trait.cpp
+++ examples/inline_mode/_2_stringable_trait.cpp
@@ -36,50 +36,48 @@ struct trait_base {
   __VA_OPT__(_detail_foreach_macro(_detail_ANYXX_TRAIT_METHOD_H, \
                                    _detail_EXPAND_LIST __VA_ARGS__))
 
-#define TRAIT_META_FUNCTION(tpl1, tpl2, tpl3, n, BASE, btpl, l)         \
-                                                                        \
-  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl1)>                    \
-  struct n;                                                             \
-                                                                        \
-  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl2)>                    \
-  struct n##_trait_default {                                            \
-    _detail_ANYXX_TRAIT_FUNCTIONS(l)                                    \
-  };                                                                    \
-                                                                        \
-  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl2)>                    \
-  struct n##_trait_map                                                  \
-      : n##_trait_default<_detail_ANYXX_TEMPLATE_ARGS(tpl2)> {};        \
-                                                                        \
-  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl1)>                    \
-  struct n : BASE<_detail_ANYXX_BASE_TEMPLATE_ACTUAL_ARGS(btpl)> {      \
-    using value_t = ErasedData;                                         \
-    using trait = n##_trait<_detail_ANYXX_TEMPLATE_ARGS(tpl3)>;         \
-    using base_t = BASE<_detail_ANYXX_BASE_TEMPLATE_ACTUAL_ARGS(btpl)>; \
-    using base_t::value_;                                               \
-    explicit(false) n(ErasedData v) : base_t(std::move(v)) {}           \
-    ErasedData& operator=(ErasedData v) {                               \
-      base_t::value_ = std::move(v);                                    \
-      return *this;                                                     \
-    }                                                                   \
-    ~n() = default;                                                     \
-    n() = default;                                                      \
-    n(n const&) = default;                                              \
-    n(n&&) = default;                                                   \
-    n& operator=(n const&) = default;                                   \
-    n& operator=(n&&) = default;                                        \
-                                                                        \
-    _detail_ANYXX_TRAIT_METHODS(l)                                      \
+#define TRAIT_META_FUNCTION(tpl1, tpl2, tpl3, n, BASE, btpl, l)               \
+                                                                              \
+  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl1)>                          \
+  struct n;                                                                   \
+                                                                              \
+  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl2)>                          \
+  struct n##_trait_default {                                                  \
+    _detail_ANYXX_TRAIT_FUNCTIONS(l)                                          \
+  };                                                                          \
+                                                                              \
+  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl2)>                          \
+  struct n##_trait : n##_trait_default<_detail_ANYXX_TEMPLATE_ARGS(tpl2)> {}; \
+                                                                              \
+  template <_detail_ANYXX_TYPENAME_PARAM_LIST(tpl1)>                          \
+  struct n : BASE<_detail_ANYXX_BASE_TEMPLATE_ACTUAL_ARGS(btpl)> {            \
+    using value_t = ErasedData;                                               \
+    using trait = n##_trait<_detail_ANYXX_TEMPLATE_ARGS(tpl3)>;               \
+    using base_t = BASE<_detail_ANYXX_BASE_TEMPLATE_ACTUAL_ARGS(btpl)>;       \
+    using base_t::value_;                                                     \
+    n(ErasedData v) : base_t(std::move(v)) {}                                 \
+    ErasedData& operator=(ErasedData v) {                                     \
+      base_t::value_ = std::move(v);                                          \
+      return *this;                                                           \
+    }                                                                         \
+    ~n() = default;                                                           \
+    n() = default;                                                            \
+    n(n const&) = default;                                                    \
+    n(n&&) = default;                                                         \
+    n& operator=(n const&) = default;                                         \
+    n& operator=(n&&) = default;                                              \
+                                                                              \
+    _detail_ANYXX_TRAIT_METHODS(l)                                            \
   };
 
 #define TRAIT_(n, BASE, l) \
-  TRAIT_META_FUNCTION((ErasedData), (T), (Model), n, BASE, (), l)
+  TRAIT_META_FUNCTION((ErasedData), (T), (ErasedData), n, BASE, (), l)
 
 #define TRAIT(n, ...) TRAIT_(n, ::anyxx::trait_base, __VA_ARGS__)
 
-#define TRAIT_TEMPLATE_(t, n, BASE, btpl, l)                                 \
-  TRAIT_META_FUNCTION(_add_head((ErasedData), t), _add_head((T), t),         \
-                      _add_head((Concrete), t), _add_head((Other), t), t, n, \
-                      BASE, btpl, l)
+#define TRAIT_TEMPLATE_(t, n, BASE, btpl, l)                         \
+  TRAIT_META_FUNCTION(_add_head((ErasedData), t), _add_head((T), t), \
+                      _add_head((ErasedData), t), n, BASE, btpl, l)
 
 #define TRAIT_TEMPLATE(t, n, l) \
   TRAIT_TEMPLATE_(t, n, ::anyxx::trait_base, (), l)
@@ -93,24 +91,26 @@ TRAIT(stringable, (ANY_METHOD(std::string, to_string, (), const)))
 template <>
 struct stringable_trait<bool> {
   static std::string to_string(bool const& value) {
-    return value ? "wahr" : "falsch"
+    return value ? "wahr" : "falsch";
   };
+};
 
-  template <>
-  struct stringable_trait<double> {
-    static std::string to_string(bool double& value) {
-      return std::format("{03.2}", value);
-    }
-  };
-
-  template <typename V>
-  void print(stringable<V> const& s) {
-    std::cout << s.to_string() << "\n";
+template <>
+struct stringable_trait<double> {
+  static std::string to_string(const double& value) {
+    return std::format("{:6.2}", value);
   }
 };
 
+template <typename V>
+void print(stringable<V> const& s) {
+  std::cout << s.to_string() << "\n";
+}
+
 }  // namespace example_2
+
 TEST_CASE("example 2 ") {
+  using namespace example_2;
   print(true);
   print(3.14);
   print(42);
