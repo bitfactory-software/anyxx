6d3e766f91d733c3e7330e2c65f151cfeebea837
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-12-07 11:16:29 +0100

    any_to_tstring_v_table_is_inline

--- test/tests_dll_mode/_any_template_dll_mode.cpp
+++ test/tests_dll_mode/_any_template_dll_mode.cpp
@@ -28,6 +28,7 @@ struct X {
 
 namespace template_test {
 
+struct any_to_tstring_v_table_is_inline {};
 ANY(any_to_tstring, (ANY_METHOD(std::string, to_string, (), const)))
 
 struct any_map_to_tstring_v_table_is_inline {};
@@ -56,6 +57,7 @@ using namespace anyxx;
 
 }  // namespace
 
+ANY_META_CLASS_STATIC(int)
 ANY_META_CLASS_STATIC(std::map<int, double>)
 ANY_META_CLASS_STATIC(std::map<std::string, int>)
 ANY_META_CLASS_STATIC(
@@ -162,12 +164,12 @@ TEST_CASE("any template test") {
 
   test_any_map_template<std::string, int>(map_string_to_int);
 
-  // auto test_any_map_to_tstring_lambda =
-  //     [](any_map_to_tstring<const_observer, std::string> map_i) {
-  //       REQUIRE(map_i.at("one").to_string() == "1");
-  //       REQUIRE(map_i.at("two").to_string() == "2");
-  //     };
-  // test_any_map_to_tstring_lambda(map_string_to_int);
+   auto test_any_map_to_tstring_lambda =
+       [](any_map_to_tstring<const_observer, std::string> map_i) {
+         REQUIRE(map_i.at("one").to_string() == "1");
+         REQUIRE(map_i.at("two").to_string() == "2");
+       };
+   test_any_map_to_tstring_lambda(map_string_to_int);
 }
 
 TEST_CASE("any template test2") {
