4d0757dc587968d297a1d0961a740f09798dc764
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-01-07 19:22:36 +0100

    wip

--- examples/inline_mode/_2b_trait_monoid.cpp
+++ examples/inline_mode/_2b_trait_monoid.cpp
@@ -8,30 +8,30 @@
 #ifdef __cpp_lib_ranges_fold
 
 // count arguments
-#define _detail_ANYXX_NARGS(...) _detail_ANYXX_NARGS_(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-#define _detail_ANYXX_NARGS_(_10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
+#define ANYXX_NARGS(...) ANYXX_NARGS_(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define ANYXX_NARGS_(_10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
 
 // utility (concatenation)
-#define _detail_ANYXX_CONC(A, B) _detail_ANYXX_CONC_(A, B)
-#define _detail_ANYXX_CONC_(A, B) A##B
+#define ANYXX_CONC(A, B) ANYXX_CONC_(A, B)
+#define ANYXX_CONC_(A, B) A##B
 
-#define _detail_ANYXX_GET_ELEM(N, ...) _detail_ANYXX_CONC(_detail_ANYXX_GET_ELEM_, N)(__VA_ARGS__)
-#define _detail_ANYXX_GET_ELEM_0(_0, ...) _0
-#define _detail_ANYXX_GET_ELEM_1(_0, _1, ...) _1
-#define _detail_ANYXX_GET_ELEM_2(_0, _1, _2, ...) _2
-#define _detail_ANYXX_GET_ELEM_3(_0, _1, _2, _3, ...) _3
-#define _detail_ANYXX_GET_ELEM_4(_0, _1, _2, _3, _4, ...) _4
-#define _detail_ANYXX_GET_ELEM_5(_0, _1, _2, _3, _4, _5, ...) _5
-#define _detail_ANYXX_GET_ELEM_6(_0, _1, _2, _3, _4, _5, _6, ...) _6
-#define _detail_ANYXX_GET_ELEM_7(_0, _1, _2, _3, _4, _5, _6, _7, ...) _7
-#define _detail_ANYXX_GET_ELEM_8(_0, _1, _2, _3, _4, _5, _6, _7, _8, ...) _8
-#define _detail_ANYXX_GET_ELEM_9(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, ...) _9
-#define _detail_ANYXX_GET_ELEM_10(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, ...) _10
+#define ANYXX_GET_ELEM(N, ...) ANYXX_CONC(ANYXX_GET_ELEM_, N)(__VA_ARGS__)
+#define ANYXX_GET_ELEM_0(_0, ...) _0
+#define ANYXX_GET_ELEM_1(_0, _1, ...) _1
+#define ANYXX_GET_ELEM_2(_0, _1, _2, ...) _2
+#define ANYXX_GET_ELEM_3(_0, _1, _2, _3, ...) _3
+#define ANYXX_GET_ELEM_4(_0, _1, _2, _3, _4, ...) _4
+#define ANYXX_GET_ELEM_5(_0, _1, _2, _3, _4, _5, ...) _5
+#define ANYXX_GET_ELEM_6(_0, _1, _2, _3, _4, _5, _6, ...) _6
+#define ANYXX_GET_ELEM_7(_0, _1, _2, _3, _4, _5, _6, _7, ...) _7
+#define ANYXX_GET_ELEM_8(_0, _1, _2, _3, _4, _5, _6, _7, _8, ...) _8
+#define ANYXX_GET_ELEM_9(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, ...) _9
+#define ANYXX_GET_ELEM_10(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, ...) _10
 
 // Get last argument - placeholder decrements by one
-#define _detail_ANYXX_GET_LAST(...) _detail_ANYXX_GET_ELEM(_detail_ANYXX_NARGS(__VA_ARGS__), _, __VA_ARGS__ ,,,,,,,,,,,)
+#define ANYXX_GET_LAST(...) ANYXX_GET_ELEM(ANYXX_NARGS(__VA_ARGS__), _, __VA_ARGS__ ,,,,,,,,,,,)
 // usage: 
-//_detail_ANYXX_GET_LAST((A), B)
+//ANYXX_GET_LAST((A), B)
 // expands to:
 //  B
 
@@ -84,7 +84,7 @@ IIF(BITAND(IS_COMPARABLE(x))(IS_COMPARABLE(y)) )(PRIMITIVE_COMPARE, 1 EAT)(x, y)
 #define ANYXX_COMPARE_auto(x) x
 
 #define ANYXX_JACKET_PARAM_TYPE(...) \
-IF(ANYXX_EQUAL(_detail_ANYXX_GET_LAST(__VA_ARGS__), auto))( auto, anyxx::jacket_param<ANYXX_UNPAREN(_detail_ANYXX_GET_ELEM_0(__VA_ARGS__))>)   
+IF(ANYXX_EQUAL(ANYXX_GET_LAST(__VA_ARGS__), auto))( auto, anyxx::jacket_param<ANYXX_UNPAREN(ANYXX_GET_ELEM_0(__VA_ARGS__))>)   
 ANYXX_JACKET_PARAM_TYPE((std::vector<int> const&), auto) -> auto
 ANYXX_JACKET_PARAM_TYPE((std::vector<int> const&)) -> anyxx::jacket_param<std::vector<int> const&>
 
