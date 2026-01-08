92d4d71918fe731bd1b5c26dfec7e52bab2d4934
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-01-08 20:17:29 +0100

    round up monoid example

--- examples/inline_mode/_2b_trait_monoid.cpp
+++ examples/inline_mode/_2b_trait_monoid.cpp
@@ -2,8 +2,10 @@
 #include <bit_factory/anyxx.hpp>
 #include <bit_factory/anyxx_std.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <list>
 #include <print>
 #include <string>
+#include <vector>
 
 #ifdef __cpp_lib_ranges_fold
 
@@ -15,35 +17,39 @@ namespace anyxx {
 namespace example_2b {
 
 ANY_EX(monoid,
-    (ANY_METHOD_DEFAULTED(anyxx::self, id, (), const, []() { return T{}; }),
-     ANY_OP_DEFAULTED(anyxx::self, +, op, (anyxx::self const&), const,
-                      [&x](auto const& r) {
-                        auto self = anyxx::trait_as<monoid>(x);
-                        return self | (std::vector{anyxx::trait_as<monoid>(
-                                          r)});  // NOLINT
-                      }),
-     ANY_OP_DEFAULTED(
-         anyxx::self, |, concat,
-         ((anyxx::any_forward_range<anyxx::self, anyxx::self,
-                                    anyxx::const_observer> const&)),
-         const,
-         [&x](const auto& r) {
-           auto self = anyxx::trait_as<monoid>(x);
-           return std::ranges::fold_left(
-               r | std::views::transform(
-                       [](auto y) { return anyxx::trait_as<monoid>(y); }),
-               self, [&](auto const& m1, auto const& m2) { return m1 + m2; });
-         }),
-     ANY_METHOD_DEFAULTED(bool, equal_to, (anyxx::self const&), const,
-                          ([&x](auto const& r) { return x == r; }))),
-    , , (friend bool operator==(any_t const& l, any_t const& r) {
-      return l.equal_to(r);
-    }))
+       (ANY_METHOD_DEFAULTED(anyxx::self, id, (), const, []() { return T{}; }),
+        ANY_OP_DEFAULTED(anyxx::self, +, op, (anyxx::self const&), const,
+                         [&x](auto const& r) {
+                           std::println("op-default {}", typeid(T).name());
+                           auto self = anyxx::trait_as<monoid>(x);
+                           return self | (std::vector{anyxx::trait_as<monoid>(
+                                             r)});  // NOLINT
+                         }),
+        ANY_OP_DEFAULTED(
+            anyxx::self, |, concat,
+            ((anyxx::any_forward_range<anyxx::self, anyxx::self,
+                                       anyxx::const_observer> const&)),
+            const,
+            [&x](const auto& r) {
+              std::println("concat-default {}", typeid(T).name());
+              auto self = anyxx::trait_as<monoid>(x);
+              return std::ranges::fold_left(
+                  r | std::views::transform(
+                          [](auto y) { return anyxx::trait_as<monoid>(y); }),
+                  self,
+                  [&](auto const& m1, auto const& m2) { return m1 + m2; });
+            }),
+        ANY_METHOD_DEFAULTED(bool, equal_to, (anyxx::self const&), const,
+                             ([&x](auto const& r) { return x == r; }))),
+       , , (friend bool operator==(any_t const& l, any_t const& r) {
+         return l.equal_to(r);
+       }))
 
 }  // namespace example_2b
 
 ANY_MODEL_MAP((int), example_2b::monoid) {
   static int concat(int self, auto const& r) {
+    std::println("concat {}", typeid(int).name());
     return monoid_trait<int>{std::ranges::fold_left(
         r, self, [&](int m1, int m2) { return m1 + m2; })};
   };
@@ -51,6 +57,7 @@ ANY_MODEL_MAP((int), example_2b::monoid) {
 
 ANY_MODEL_MAP((std::string), example_2b::monoid) {
   static std::string op(std::string const& self, std::string const& r) {
+    std::println("op {}", typeid(std::string).name());
     return self + r;
   };
 };
@@ -58,9 +65,7 @@ ANY_MODEL_MAP((std::string), example_2b::monoid) {
 namespace example_2b {
 
 template <anyxx::is_any Monoid>
-void test_monoid(
-    Monoid const& m,
-    anyxx::any_forward_range<Monoid, Monoid, anyxx::const_observer> r) {
+void test_monoid(Monoid const& m, std::ranges::forward_range auto const& r) {
   auto id = m.id();
   using type_1 = decltype(m + id + m);
   using type_2 = decltype(m + (m + id));
@@ -74,6 +79,16 @@ void test_monoid(
                   return m1 + m2;
                 });
   CHECK(c2);
+}
+
+anyxx::any_forward_range<monoid<anyxx::value>, monoid<anyxx::value>,
+                         anyxx::value>
+make_a_range(bool use_list) {
+  using namespace std::string_literals;
+  if (use_list)
+    return std::list<monoid<anyxx::value>>{{"2"s}, {"3"s}};
+  else
+    return std::vector<monoid<anyxx::value>>{{"2"s}, {"3"s}};
 }
 
 }  // namespace example_2b
@@ -92,8 +107,8 @@ TEST_CASE("example 2b monoid ") {
       trait_as<monoid>("1"s),
       std::vector<monoid_trait<std::string>>{{"2"s}, {"3"s}});
 
-  test_monoid<monoid<anyxx::value>>(
-      "1"s, std::vector<monoid<anyxx::value>>{{"2"s}, {"3"s}});
+  test_monoid<monoid<anyxx::value>>("1"s, make_a_range(true));
+  test_monoid<monoid<anyxx::value>>("1"s, make_a_range(false));
 }
 
 #endif
