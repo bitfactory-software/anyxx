71535890e3fd87838eac6893ff1cf8f48a61b0a3
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-01-09 18:53:12 +0100

    drop unnecessary varaint code in _detail_ANYXX_METHOD

--- bit_factory/anyxx.hpp
+++ bit_factory/anyxx.hpp
@@ -279,73 +279,62 @@ static_assert(std::same_as<ANYXX_UNPAREN((int)), int>);
     }                                                                   \
   };
 
-#define _detail_ANYXX_METHOD(overload, type, name, name_ext, exact_const,      \
-                             const_, map_body, ...)                            \
-  overload decltype(auto) name_ext(__VA_OPT__(                                 \
-      _detail_ANYXX_JACKET_PARAM_LIST(a, _sig, __VA_ARGS__))) const_           \
-    requires(::anyxx::const_correct_call_for_erased_data<                      \
-             void const_*, erased_data_t, exact_const>)                        \
-  {                                                                            \
-    if constexpr (std::same_as<anyxx::vany_dispatch, Dispatch>) {              \
-      using variant_t = erased_data_t;                                         \
-      using vany_any_t = std::variant_alternative_t<0, variant_t>;             \
-      return std::visit(                                                       \
-          anyxx::overloads{                                                    \
-              [&]<typename ValueInVariant>(                                    \
-                  ValueInVariant&& value_in_variant) {                         \
-                return static_dispatch_map_t<std::decay_t<ValueInVariant>>::   \
-                    name(std::forward<ValueInVariant>(value_in_variant)        \
-                             __VA_OPT__(, )                                    \
-                                 __VA_OPT__(_detail_ANYXX_FORWARD_PARAM_LIST(  \
-                                     a, _sig, __VA_ARGS__)));                  \
-              },                                                               \
-              [&](vany_any_t const_& any) {                                    \
-                return any.name(__VA_OPT__(                                    \
-                    _detail_ANYXX_FORWARD_PARAM_LIST(a, _sig, __VA_ARGS__)));  \
-              }},                                                              \
-          base_t::erased_data_);                                               \
-    } else {                                                                   \
-      if constexpr (std::same_as<anyxx::trait, Dispatch>) {                    \
-        using traited_t = typename erased_data_t::value_t;                     \
-        if constexpr (anyxx::is_variant<traited_t>) {                          \
-          return std::visit(                                                   \
-              [&]<typename Vx>(Vx&& vx) {                                      \
-                return static_dispatch_map_t<std::decay_t<Vx>>::name(          \
-                    std::forward<Vx>(vx) __VA_OPT__(, )                        \
-                        __VA_OPT__(_detail_ANYXX_FORWARD_PARAM_LIST(           \
-                            a, _sig, __VA_ARGS__)));                           \
-              },                                                               \
-              base_t::erased_data_.value_);                                    \
-        } else {                                                               \
-          if constexpr (std::same_as<void, ANYXX_UNPAREN(type)>) {             \
-            return static_dispatch_map_t<T>::name(                             \
-                base_t::erased_data_.value_ __VA_OPT__(, )                     \
-                    __VA_OPT__(_detail_ANYXX_FORWARD_JACKET_PARAM_LIST_TO_MAP( \
-                        a, _sig, __VA_ARGS__)));                               \
-          } else {                                                             \
-            return ANYXX_JACKET_RETURN(type)::forward(                         \
-                static_dispatch_map_t<T>::name(                                \
-                    base_t::erased_data_.value_ __VA_OPT__(, ) __VA_OPT__(     \
-                        _detail_ANYXX_FORWARD_JACKET_PARAM_LIST_TO_MAP(        \
-                            a, _sig, __VA_ARGS__))),                           \
-                *this);                                                        \
-          }                                                                    \
-        }                                                                      \
-      } else {                                                                 \
-        if constexpr (std::same_as<void, ANYXX_UNPAREN(type)>) {               \
-          return get_v_table_ptr()->name(                                      \
-              anyxx::get_void_data_ptr(base_t::erased_data_) __VA_OPT__(       \
-                  , _detail_ANYXX_FORWARD_PARAM_LIST(a, _sig, __VA_ARGS__)));  \
-        } else {                                                               \
-          return ANYXX_JACKET_RETURN(type)::forward(                           \
-              get_v_table_ptr()->name(                                         \
-                  anyxx::get_void_data_ptr(base_t::erased_data_)               \
-                      __VA_OPT__(, _detail_ANYXX_FORWARD_PARAM_LIST(           \
-                                       a, _sig, __VA_ARGS__))),                \
-              *this);                                                          \
-        }                                                                      \
-      }                                                                        \
-    }                                                                          \
+#define _detail_ANYXX_METHOD(overload, type, name, name_ext, exact_const,     \
+                             const_, map_body, ...)                           \
+  overload decltype(auto) name_ext(__VA_OPT__(                                \
+      _detail_ANYXX_JACKET_PARAM_LIST(a, _sig, __VA_ARGS__))) const_          \
+    requires(::anyxx::const_correct_call_for_erased_data<                     \
+             void const_*, erased_data_t, exact_const>)                       \
+  {                                                                           \
+    if constexpr (std::same_as<anyxx::vany_dispatch, Dispatch>) {             \
+      using variant_t = erased_data_t;                                        \
+      using vany_any_t = std::variant_alternative_t<0, variant_t>;            \
+      return std::visit(                                                      \
+          anyxx::overloads{                                                   \
+              [&]<typename ValueInVariant>(                                   \
+                  ValueInVariant&& value_in_variant) {                        \
+                return static_dispatch_map_t<std::decay_t<ValueInVariant>>::  \
+                    name(std::forward<ValueInVariant>(value_in_variant)       \
+                             __VA_OPT__(, )                                   \
+                                 __VA_OPT__(_detail_ANYXX_FORWARD_PARAM_LIST( \
+                                     a, _sig, __VA_ARGS__)));                 \
+              },                                                              \
+              [&](vany_any_t const_& any) {                                   \
+                return any.name(__VA_OPT__(                                   \
+                    _detail_ANYXX_FORWARD_PARAM_LIST(a, _sig, __VA_ARGS__))); \
+              }},                                                             \
+          base_t::erased_data_);                                              \
+    } else {                                                                  \
+      if constexpr (std::same_as<anyxx::trait, Dispatch>) {                   \
+        using traited_t = typename erased_data_t::value_t;                    \
+        if constexpr (std::same_as<void, ANYXX_UNPAREN(type)>) {              \
+          return static_dispatch_map_t<T>::name(                              \
+              base_t::erased_data_.value_ __VA_OPT__(, )                      \
+                  __VA_OPT__(_detail_ANYXX_FORWARD_JACKET_PARAM_LIST_TO_MAP(  \
+                      a, _sig, __VA_ARGS__)));                                \
+        } else {                                                              \
+          return ANYXX_JACKET_RETURN(type)::forward(                          \
+              static_dispatch_map_t<T>::name(                                 \
+                  base_t::erased_data_.value_ __VA_OPT__(, ) __VA_OPT__(      \
+                      _detail_ANYXX_FORWARD_JACKET_PARAM_LIST_TO_MAP(         \
+                          a, _sig, __VA_ARGS__))),                            \
+              *this);                                                         \
+        }                                                                     \
+      } else {                                                                \
+        if constexpr (std::same_as<void, ANYXX_UNPAREN(type)>) {              \
+          return get_v_table_ptr()->name(                                     \
+              anyxx::get_void_data_ptr(base_t::erased_data_) __VA_OPT__(      \
+                  , _detail_ANYXX_FORWARD_PARAM_LIST(a, _sig, __VA_ARGS__))); \
+        } else {                                                              \
+          return ANYXX_JACKET_RETURN(type)::forward(                          \
+              get_v_table_ptr()->name(                                        \
+                  anyxx::get_void_data_ptr(base_t::erased_data_)              \
+                      __VA_OPT__(, _detail_ANYXX_FORWARD_PARAM_LIST(          \
+                                       a, _sig, __VA_ARGS__))),               \
+              *this);                                                         \
+        }                                                                     \
+      }                                                                       \
+    }                                                                         \
   }
 
 #define _detail_ANYXX_MAP_FUNCTIONS(...)                     \
