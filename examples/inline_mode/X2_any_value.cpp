2dc2bc745ff14ce9c573f40b09e92a19958eebd2
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-01-11 21:29:29 +0100

    more _X2_any_value tests

--- examples/inline_mode/X2_any_value.cpp
+++ examples/inline_mode/X2_any_value.cpp
@@ -6,7 +6,7 @@ using namespace anyxx;
 
 namespace {
 
-//ANY(any, , , )
+// ANY(any, , , )
 
 }  // namespace
 
@@ -15,4 +15,13 @@ TEST_CASE("example X2/ any value") {
   static_assert(anyxx::is_any<decltype(a1)>);
   CHECK(*unerase_cast<std::string>(a1) == "hello world");
   CHECK(get_meta_data(a1).get_type_info() == typeid(std::string));
+  auto a2 = a1;  // copy
+  CHECK(*unerase_cast<std::string>(a2) == "hello world");
+  CHECK(get_meta_data(a2).get_type_info() == typeid(std::string));
+  a2 = 42;
+  CHECK(*unerase_cast<std::string>(a1) == "hello world");
+  CHECK(get_meta_data(a1).get_type_info() == typeid(std::string));
+  CHECK(*unerase_cast<int>(a2) == 42);
+  CHECK(get_meta_data(a2).get_type_info() == typeid(int));
 }
+
