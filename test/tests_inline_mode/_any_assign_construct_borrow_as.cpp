9265cfa54de50404399e4d44bf2e3dbb5bdcd0ba
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-01-13 20:33:07 +0100

    assign construct borrow_as for value

--- test/tests_inline_mode/_any_assign_construct_borrow_as.cpp
+++ test/tests_inline_mode/_any_assign_construct_borrow_as.cpp
@@ -121,4 +121,28 @@ TEST_CASE("assign construct borrow_as") {
     to_t vv4{vv1};
     CHECK(get_void_data_ptr(*lock(vv4)) == get_void_data_ptr(vv1));
   }
+  {
+    using from_t = any<value>;
+    using to_t = any<const_observer>;
+    static_assert(borrowable_from<to_t::erased_data_t, from_t::erased_data_t>);
+    auto vv1 = from_t{s1};
+    auto vv2 = borrow_as<to_t>(vv1);
+    CHECK(get_void_data_ptr(vv1) == get_void_data_ptr(*vv2));
+    to_t vv3 = vv1;
+    CHECK(get_void_data_ptr(vv3) == get_void_data_ptr(vv1));
+    to_t vv4{vv1};
+    CHECK(get_void_data_ptr(vv4) == get_void_data_ptr(vv1));
+  }
+  {
+    using from_t = any<value>;
+    using to_t = any<mutable_observer>;
+    static_assert(borrowable_from<to_t::erased_data_t, from_t::erased_data_t>);
+    auto vv1 = from_t{s1};
+    auto vv2 = borrow_as<to_t>(vv1);
+    CHECK(get_void_data_ptr(vv1) == get_void_data_ptr(*vv2));
+    to_t vv3 = vv1;
+    CHECK(get_void_data_ptr(vv3) == get_void_data_ptr(vv1));
+    to_t vv4{vv1};
+    CHECK(get_void_data_ptr(vv4) == get_void_data_ptr(vv1));
+  }
 }
