4eaca97c32af319bd631e2a4b1ceaca1a5e60db4
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-01-30 21:09:16 +0000

    Deploying to gh-pages from @ bitfactory-software/anyxx@3aa5124f96da05cf581eae7c43470d3a608b09c4 🚀

---
+++ group__model__map__macros.js
@@ -0,0 +1,5 @@
+var group__model__map__macros =
+[
+    [ "ANY_MODEL_MAP", "group__model__map__macros.html#ga826d8afebc4f1e8bba64b431d9f7d611", null ],
+    [ "ANY_TEMPLATE_MODEL_MAP", "group__model__map__macros.html#ga5e76f3ddd3b53ae5041dfaaec4b2288d", null ]
+];
--- topics.js
+++ topics.js
@@ -4,5 +4,6 @@ var topics =
     [ "Casts", "group__casts.html", "group__casts" ],
     [ "Runtime data macros", "group__runtime__macros.html", "group__runtime__macros" ],
     [ "TRAIT... and ANY_ macros", "group__trait__macros.html", "group__trait__macros" ],
-    [ "ANY_FN... and ANY_OP macros", "group__fn__macros.html", "group__fn__macros" ]
+    [ "ANY_FN... and ANY_OP macros", "group__fn__macros.html", "group__fn__macros" ],
+    [ "MODEL_MAP macros", "group__model__map__macros.html", "group__model__map__macros" ]
 ];
