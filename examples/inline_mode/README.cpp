fb121fb2d7b9bc9309d24408d1446f84c5ac5fa2
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-02-04 14:41:15 +0100

    fix copyright

--- examples/inline_mode/README.cpp
+++ examples/inline_mode/README.cpp
@@ -1,7 +1,7 @@
 // <!--
 // The MIT License (MIT)
 //
-// Copyright (c) 2024 Kris Jusiak <kris@jusiak.net>
+// Copyright (c) 2026 Bit Factory Software GmbH
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
