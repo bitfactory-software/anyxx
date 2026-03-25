c0e62eefb464fa670c421ae9b5642d3626256564
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-03-25 18:45:54 +0100

    simplify partial_equality example

--- examples/inline_mode/_2f_trait_partial_equality.cpp
+++ examples/inline_mode/_2f_trait_partial_equality.cpp
@@ -29,22 +29,19 @@ static_assert(is_partial_equality_model<double>);
 static_assert(is_partial_equality_model<bool>);
 static_assert(is_partial_equality_model<std::string>);
 
-template <typename T>
-void test_partial_equality_(
-    anyxx::any<anyxx::using_<T>, partial_equality> const& a,
-    anyxx::any<anyxx::using_<T>, partial_equality> const& b) {
+template <anyxx::is_proxy Proxy>
+void test_partial_equality_(anyxx::any<Proxy, partial_equality> const& a,
+                            anyxx::any<Proxy, partial_equality> const& b) {
   CHECK((a == b) == (b == a));
   CHECK((a != b) == (b != a));
 }
 template <is_partial_equality_model T>
 void test_partial_equality(T const& a, T const& b) {
   using namespace anyxx;
-  test_partial_equality_(trait_as<partial_equality>(a),
-                         trait_as<partial_equality>(b));
+  test_partial_equality_<anyxx::using_<T>>(a, b);
 }
 
 }  // namespace lib_2f
-
 
 namespace app_2f {
 
@@ -72,7 +69,24 @@ ANY_MODEL_MAP((app_2f::b_type), lib_2f::partial_equality) {
 };
 static_assert(lib_2f::is_partial_equality_model<app_2f::b_type>);
 
-TEST_CASE("partial_equality") {
+TEST_CASE("partial_equality static") {
   lib_2f::test_partial_equality(1, 1);
   lib_2f::test_partial_equality(app_2f::b_type{"A"}, app_2f::b_type{"B"});
+}
+
+TEST_CASE("partial_equality dynamic") {
+  using any_partial_equality = anyxx::any<anyxx::val, lib_2f::partial_equality>;
+  std::vector<std::pair<any_partial_equality, any_partial_equality>> v{
+      {app_2f::b_type{"A"}, app_2f::b_type{"A"}},
+      {app_2f::b_type{"A"}, app_2f::b_type{"B"}},
+      {3, 3},
+      {3, 4},
+      {3.14, 3.14},
+      {3.14, 2.71},
+      {"hello", "hello"},
+      {"hello", "world"},
+  };
+  for (auto const& [a, b] : v) {
+    lib_2f::test_partial_equality_(a, b);
+  }
 }
