92f8c2b78591046e2bd38ae9d111e9338d8fc520
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-06-03 19:12:34 +0000

    Deploying to gh-pages from @ bitfactory-software/anyxx@6b25e107d92501173ba9c684f0a06e70c48957dc 🚀

--- classanyxx_1_1any.js
+++ classanyxx_1_1any.js
@@ -2,6 +2,6 @@ var classanyxx_1_1any =
 [
     [ "any", "classanyxx_1_1any.html#a440c04e593e924f0518702df4b55cf60", null ],
     [ "any", "classanyxx_1_1any.html#a2dee555d6fc08f2449ab24e9d11a97d3", null ],
-    [ "explicit", "classanyxx_1_1any.html#a60dc90fabfa83f104ff9074f908e4c55", null ],
-    [ "explicit", "classanyxx_1_1any.html#a104f1a86d9d5e541d635e81a2e10d828", null ]
+    [ "explicit", "classanyxx_1_1any.html#a676e41523a0c3958dd5c23d1eef2c44d", null ],
+    [ "explicit", "classanyxx_1_1any.html#a759cc821d48aac64b68261769263ef0d", null ]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['explicit_0',['explicit',['../classanyxx_1_1any.html#a104f1a86d9d5e541d635e81a2e10d828',1,'anyxx::any::explicit(false) any(ConstructedWith &amp;&amp;const ructed_with)'],['../classanyxx_1_1any.html#a60dc90fabfa83f104ff9074f908e4c55',1,'anyxx::any::explicit(false) any(ConstructedWith &amp;&amp;const ructed_with LIFETIMEBOUND)']]]
+  ['explicit_0',['explicit',['../classanyxx_1_1any.html#a759cc821d48aac64b68261769263ef0d',1,'anyxx::any::explicit(false) any(ConstructedWith &amp;&amp;const ructed_with)'],['../classanyxx_1_1any.html#a676e41523a0c3958dd5c23d1eef2c44d',1,'anyxx::any::explicit(false) any(ConstructedWith &amp;&amp;const ructed_with LIFETIMEBOUND)']]]
 ];
