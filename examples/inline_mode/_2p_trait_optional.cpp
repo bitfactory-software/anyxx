6b25e107d92501173ba9c684f0a06e70c48957dc
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-06-03 21:11:38 +0200

    _2p_trait_optional works initially

--- examples/inline_mode/_2p_trait_optional.cpp
+++ examples/inline_mode/_2p_trait_optional.cpp
@@ -1,20 +1,30 @@
 #include <bit_factory/anyxx.hpp>
 #include <catch2/catch_test_macros.hpp>
-#include <string>
+#include <variant>
 
-namespace _2p {
+namespace _2p_lib {
 
-TRAIT(nullable,
-      (ANY_FN_DEF(public, bool, has_value, (), const, []() { return true; }),
-       ANY_FN_DEF(public, T, get_value, (), const, [x]() { return x; })))
+template <typename T>
+struct nullable_default_rep : std::variant<std::monostate, T> {
+  using std::variant<std::monostate, T>::variant;
+};
+TRAIT(nullable, (ANY_FN_DEF(public, bool, has_value, (), const,
+                            [x]() { return std::holds_alternative<T>(x); }),
+                 ANY_FN_DEF(public, T, get_value, (), const,
+                            [x]() { return std::get<T>(x); })))
 
-}
+template <typename T>
+using optional = anyxx::using_<T>::template as<nullable>;
+}  // namespace _2p_lib
 
 TEST_CASE("test optional") {
   using namespace anyxx;
-  using namespace _2p;
-   
-  using_<int>::as<nullable> optional_int{42};
-  CHECK(optional_int.has_value());
-  CHECK(optional_int.get_value() == 42);
+  using namespace _2p_lib;
+
+  optional<int> i1{42};
+  CHECK(i1.has_value());
+  CHECK(i1.get_value() == 42);
+
+  optional<int> i2;
+  CHECK(!i2.has_value());
 }
