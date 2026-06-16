626d5e84dc8688695921945e10659adcc52707b4
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-06-16 17:21:30 +0000

    Deploying to gh-pages from @ bitfactory-software/anyxx@16ca26b3ea14239639269bb4b34364af2170fd93 🚀

--- anyxx_8hpp.js
+++ anyxx_8hpp.js
@@ -2,10 +2,10 @@ var anyxx_8hpp =
 [
     [ "anyxx::undefined", "structanyxx_1_1undefined.html", null ],
     [ "anyxx::observeable_v_table", "structanyxx_1_1observeable__v__table.html", "structanyxx_1_1observeable__v__table" ],
-    [ "anyxx::observeable_rtti_v_table", "structanyxx_1_1observeable__rtti__v__table.html", "structanyxx_1_1observeable__rtti__v__table" ],
     [ "anyxx::any&lt; Proxy, Trait &gt;", "classanyxx_1_1any.html", "classanyxx_1_1any" ],
     [ "anyxx::using_&lt; Value &gt;", "structanyxx_1_1using__.html", "structanyxx_1_1using__" ],
     [ "anyxx::trait_class&lt; Type &gt;", "structanyxx_1_1trait__class.html", null ],
+    [ "anyxx::observeable_rtti_v_table", "structanyxx_1_1observeable__rtti__v__table.html", "structanyxx_1_1observeable__rtti__v__table" ],
     [ "anyxx::key&lt; Tag &gt;", "structanyxx_1_1key.html", null ],
     [ "anyxx::factory&lt; Any, Key, Args &gt;", "classanyxx_1_1factory.html", null ],
     [ "anyxx::members&lt; InObject &gt;", "structanyxx_1_1members.html", null ],
