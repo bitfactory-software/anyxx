5c6237491bcec5cb17c477596a91cfaf934f13ae
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-06-18 16:47:21 +0000

    Deploying to gh-pages from @ bitfactory-software/anyxx@ce858b8fb04e6e298a7f52632c989ef03250bc1e 🚀

--- search/classes_0.js
+++ search/classes_0.js
@@ -11,8 +11,9 @@ var searchData=
   ['any_3c_20function_3c_20int_28double_29_2c_20mutable_5f_20_3e_2c_20mutref_20_3e_8',['any&lt; function&lt; int(double), mutable_ &gt;, mutref &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
   ['any_3c_20function_3c_20int_28double_29_2c_20mutable_5f_20_3e_2c_20using_5f_3c_20functor_20_26_20_3e_20_3e_9',['any&lt; function&lt; int(double), mutable_ &gt;, using_&lt; functor &amp; &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
   ['any_3c_20function_3c_20int_28double_29_2c_20mutable_5f_20_3e_2c_20using_5f_3c_20functor_20const_20_26_20_3e_20_3e_10',['any&lt; function&lt; int(double), mutable_ &gt;, using_&lt; functor const &amp; &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
-  ['any_3c_20trait_2c_20trait_5fclass_3c_20type_20_3e_20_3e_11',['any&lt; Trait, trait_class&lt; Type &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
-  ['any_3c_20trait_2c_20using_5f_3c_20type_20_3e_20_3e_12',['any&lt; Trait, using_&lt; Type &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
-  ['any_3c_20trait_2c_20using_5f_3c_20value_20_3e_20_3e_13',['any&lt; Trait, using_&lt; Value &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
-  ['any_3c_20trait_3c_20ts_2e_2e_2e_20_3e_2c_20using_5f_3c_20type_20_3e_20_3e_14',['any&lt; Trait&lt; Ts... &gt;, using_&lt; Type &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]]
+  ['any_3c_20trait_2c_20otherproxy_20_3e_11',['any&lt; Trait, OtherProxy &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
+  ['any_3c_20trait_2c_20trait_5fclass_3c_20type_20_3e_20_3e_12',['any&lt; Trait, trait_class&lt; Type &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
+  ['any_3c_20trait_2c_20using_5f_3c_20type_20_3e_20_3e_13',['any&lt; Trait, using_&lt; Type &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
+  ['any_3c_20trait_2c_20using_5f_3c_20value_20_3e_20_3e_14',['any&lt; Trait, using_&lt; Value &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]],
+  ['any_3c_20trait_3c_20ts_2e_2e_2e_20_3e_2c_20using_5f_3c_20type_20_3e_20_3e_15',['any&lt; Trait&lt; Ts... &gt;, using_&lt; Type &gt; &gt;',['../classanyxx_1_1any.html',1,'anyxx']]]
 ];
