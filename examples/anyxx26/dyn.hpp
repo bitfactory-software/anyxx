99cae127c63a7f06a030e91fa1ee4b496456613f
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-16 18:40:10 +0200

    Refactor dyn_self_val_t to use default_proxy_t

--- examples/anyxx26/dyn.hpp
+++ examples/anyxx26/dyn.hpp
@@ -13,22 +13,6 @@ namespace anyxx26 {
 template <template <typename, typename, typename...> typename Trait, typename... Args>
 struct dyn;
 
-template <template <typename, typename, typename...> typename Trait, typename... Args>
-using dyn_self_val_t = dyn<Trait, anyxx::val<>, Args...>;
-
-template <template <typename, typename, typename...> typename Trait, typename... Args>
-using dyn_self_cref_t = dyn<Trait, anyxx::cref, Args...>;
-
-template <template <typename, typename, typename...> typename Trait, typename... Args>
-using dyn_self_mutref_t = dyn<Trait, anyxx::mutref, Args...>;
-
-template <typename ...Ts>
-struct select_last {
-    using type = typename decltype((std::enable_if<true, Ts>{}, ...))::type;
-};
-template <typename ...Ts>
-using proxy_from_args_t = typename select_last<Ts...>::type;
-
 struct default_t {};
 constexpr static inline default_t defaulted = {};
 
@@ -45,14 +29,39 @@ using self_const_correct_t = std::conditional_t<
 
 template<std::meta::info TraitTemplate, typename V, std::meta::info... Args>
 consteval std::meta::info trait_model_map(){
-    return substitute(TraitTemplate, {^^V, ^^model_map, Args...});;
+    return substitute(TraitTemplate, { ^^V,^^ model_map, Args... });;
 }
 template<std::meta::info TraitTemplate, std::meta::info... Args>
 consteval std::meta::info trait_declaration(){
-    return substitute(TraitTemplate, {^^void*, ^^declaration, Args...});;
+    return substitute(TraitTemplate, { ^^void*,^^ declaration, Args... });;
 }
 template<template<typename, typename...> typename TraitTemplate, typename... Args>
 using trait_declaration_t = TraitTemplate<void*, declaration, Args...>;
+
+template <template <typename, typename, typename...> typename Trait, typename... Args>
+concept specifies_default_proxy_t = requires { typename trait_declaration_t<Trait, Args...>::default_proxy_t; };
+
+template <template <typename, typename, typename...> typename Trait, typename... Args>
+consteval std::meta::info compute_default_proxy_t() {
+    if constexpr(specifies_default_proxy_t<Trait, Args...>) {
+        return ^^typename trait_declaration_t<Trait, Args...>::default_proxy_t;
+    } else {
+        return ^^anyxx::cref;
+    }
+}
+
+template <template <typename, typename, typename...> typename Trait, typename... Args>
+using default_proxy_t = [:compute_default_proxy_t<Trait, Args...>():];
+
+template <template <typename, typename, typename...> typename Trait, typename... Args>
+using dyn_self_val_t = dyn<Trait, default_proxy_t<Trait, Args...>, Args...>;
+
+template <template <typename, typename, typename...> typename Trait, typename... Args>
+using dyn_self_cref_t = dyn<Trait, anyxx::cref, Args...>;
+
+template <template <typename, typename, typename...> typename Trait, typename... Args>
+using dyn_self_mutref_t = dyn<Trait, anyxx::mutref, Args...>;
+
 
 template <typename R, typename V>
 consteval std::meta::info translate_impl_return_type() {
--- examples/anyxx26/rangesxx.hpp
+++ examples/anyxx26/rangesxx.hpp
@@ -6,6 +6,8 @@ namespace anyxx26::rangesxx {
 
 template <typename Self, typename Trait, typename Value>
 struct input_iterator : save_copyable<Self, Trait> {
+    using default_proxy_t = anyxx::val<std::true_type>;
+
     Value& operator*();
     anyxx::self& operator++();
     bool operator==(anyxx::self const&) const;
@@ -37,7 +39,7 @@ struct bidirectional_iterator : forward_iterator<Self, Trait, Value> {
 };
 
 template <typename Self, typename Trait, typename Value>
-struct random_access_iterator : forward_iterator<Self, Trait, Value> {
+struct random_access_iterator : bidirectional_iterator<Self, Trait, Value> {
     bool operator<(anyxx::self const&) const;
     bool operator>(anyxx::self const&) const;
     bool operator<=(anyxx::self const&) const;
@@ -48,12 +50,19 @@ struct random_access_iterator : forward_iterator<Self, Trait, Value> {
     anyxx::self operator-(std::ptrdiff_t) const;
     anyxx::self operator+(std::ptrdiff_t) const;
 
+    std::ptrdiff_t operator-(anyxx::self const&) const;
+
     Value& operator[](std::ptrdiff_t) const;
 
-    struct typenames : forward_iterator<Self, Trait, Value>::typenames {
+    struct typenames : bidirectional_iterator<Self, Trait, Value>::typenames {
         using iterator_category = std::random_access_iterator_tag;
     };
 };
+
+template <typename Value>
+auto operator+(std::ptrdiff_t n, dyn<random_access_iterator, anyxx::val<std::true_type>, Value> const& it) {
+  return it + n;
+}
 
 }
 
