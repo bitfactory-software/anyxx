dd8fbd4c7fc524a2a2089f29796ef30ce242546b
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-17 13:46:39 +0200

    get_member... no longer template

--- examples/anyxx26/dyn.hpp
+++ examples/anyxx26/dyn.hpp
@@ -406,12 +406,12 @@ void set_v_table_members(VTable* v_table) {
 
     template for(constexpr auto interface_m : define_static_array(members_of(trait_declaration<Trait, Args...>(), ctx))) {
         if constexpr(has_identifier(interface_m) && is_type(interface_m) && annotations_of_with_type(interface_m, ^^ v_table_data_t).size() > 0) {
-            constexpr auto m = anyxx26::meta::get_member<FunctionPointers, interface_m>();
+            constexpr auto m = anyxx26::meta::get_member(FunctionPointers, interface_m);
             v_table->[:m:] = [:interface_m:]::template init<Concrete>(v_table);
         }
         if constexpr((has_identifier(interface_m) && is_function(interface_m))
             || (is_user_declared(interface_m) && is_operator_function(interface_m))) {
-            constexpr auto f = anyxx26::meta::get_member<FunctionPointers, interface_m>();
+            constexpr auto f = anyxx26::meta::get_member(FunctionPointers, interface_m);
             constexpr auto m = find_function_impl<Trait, Concrete, interface_m, Args...>();
             v_table->[:f:] = [:make_vfimpl<Concrete, interface_m, m, dyn_self_val, dyn_self_cref, dyn_self_mutref>():];
         }
@@ -667,7 +667,7 @@ struct dyn_facade_call {
     auto v_table_ptr = base->v_table_;
     using fptrs_t = typename v_table_t::fptrs_t;
     auto fptrs = static_cast<fptrs_t*>(v_table_ptr);
-    auto constexpr vf = anyxx26::meta::get_member<^^fptrs_t, f>();
+    auto constexpr vf = anyxx26::meta::get_member(^^fptrs_t, f);
     auto x = anyxx::get_proxy_ptr(base->proxy_, v_table_ptr);
     if constexpr(std::same_as<typename [:return_type_of(f):], anyxx::self&>) {
         fptrs->[:vf:](x, std::forward<Args>(args)...);
--- examples/anyxx26/meta/utilities.hpp
+++ examples/anyxx26/meta/utilities.hpp
@@ -26,27 +26,25 @@ struct outer {
 template <std::meta::info... Ms>
 using to_struct = outer<Ms...>::inner;
 
-template <std::meta::info Struct, auto id>
-consteval std::meta::info get_member_by_id() {
+consteval std::meta::info get_member_by_id(std::meta::info in, auto id) {
     constexpr auto ctx = std::meta::access_context::current();
-    template for(constexpr auto m : define_static_array(members_of(Struct, ctx))) {
-        if constexpr(has_identifier(m) && identifier_of(m) == std::string_view{id}) {
+    for(auto m : members_of(in, ctx)) {
+        if (has_identifier(m) && identifier_of(m) == std::string_view{id}) {
             return m;
         }
-        if constexpr(is_operator_function(m) && anyxx26::meta::enum_to_string(operator_of(m)) == std::string_view{ id }) {
+        if (is_operator_function(m) && anyxx26::meta::enum_to_string(operator_of(m)) == std::string_view{ id }) {
             return m;
         }
     }
     return {};
 }
 
-template <std::meta::info Struct, std::meta::info Other>
-consteval std::meta::info get_member() {
-    if constexpr(has_identifier(Other)) {
-        return get_member_by_id<Struct, define_static_string(identifier_of(Other))>();
+consteval std::meta::info get_member(std::meta::info in, std::meta::info other_member) {
+    if (has_identifier(other_member)) {
+        return get_member_by_id(in, define_static_string(identifier_of(other_member)));
     } 
-    if constexpr(is_operator_function(Other)) {
-      return get_member_by_id<Struct, define_static_string(anyxx26::meta::enum_to_string(operator_of(Other)))>();
+    if (is_operator_function(other_member)) {
+      return get_member_by_id(in, define_static_string(anyxx26::meta::enum_to_string(operator_of(other_member))));
     }
     return {};
 }
