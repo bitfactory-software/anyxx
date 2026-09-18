29ca95627363225b364062362f45377f55a6e93c
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-18 10:51:19 +0200

    Refactor loop to iterate directly over Trait members

--- examples/anyxx26/dyn.hpp
+++ examples/anyxx26/dyn.hpp
@@ -429,7 +429,7 @@ using base_v_table_t = anyxx::observeable::v_table_t;
 template <std::meta::info Trait, std::meta::info interface_function>
 consteval std::optional<std::meta::info> find_function_impl_in() {
     constexpr auto ctx = std::meta::access_context::current();
-    for(auto m : define_static_array(members_of(Trait, ctx))) {
+    for(auto m : members_of(Trait, ctx)) {
         if (interface_function == m){
             return { m };
         }
