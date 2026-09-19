f65c86e95880a1fb00f378cecc87c88550369d9a
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-19 19:48:48 +0200

    get_member with signature matching

--- examples/anyxx26/dyn.hpp
+++ examples/anyxx26/dyn.hpp
@@ -297,9 +297,9 @@ using base_v_table_t = anyxx::observeable::v_table_t;
 
 template <std::meta::info InterfaceFunction, std::meta::info TraitTemplate, typename V, std::meta::info... Args>
 consteval std::meta::info find_function_impl() {
-  if constexpr(constexpr auto found_in_impl = get_member(trait_model_map<TraitTemplate, V, Args...>(), InterfaceFunction); found_in_impl != std::meta::info{}) {
+  if constexpr(constexpr auto found_in_impl = get_implementation_member(trait_model_map<TraitTemplate, V, Args...>(), ^^V, InterfaceFunction); found_in_impl != std::meta::info{}) {
     return found_in_impl;
-  } else if constexpr(constexpr auto found_in_base = get_member(trait_declaration<TraitTemplate, Args...>(), InterfaceFunction); found_in_base != std::meta::info{}) {
+  } else if constexpr(constexpr auto found_in_base = get_implementation_member(trait_declaration<TraitTemplate, Args...>(), ^^V, InterfaceFunction); found_in_base != std::meta::info{}) {
       return found_in_base;
   } else {
       static_assert(false, "Function not found in impl trait or base trait");
--- examples/anyxx26/dyn/signature_translation.hpp
+++ examples/anyxx26/dyn/signature_translation.hpp
@@ -102,8 +102,40 @@ consteval std::meta::info get_member_by_decorated_name(std::meta::info in, std::
     return {};
 }
 
-consteval std::meta::info get_member(std::meta::info in, std::meta::info other_member) {
-    return get_member_by_decorated_name(in, decorated_name_of(other_member));
+consteval bool same_signature(std::meta::info self_t, auto const& candidate_params, auto const& declaration_params) {
+    if (candidate_params.size() != declaration_params.size()) {
+        return false;
+    }
+    for (auto i : std::views::iota(0u, candidate_params.size())) {
+      auto declaration_type = type_of(declaration_params[i]);
+      auto candidate_type = type_of(candidate_params[i]);
+      if (candidate_type == declaration_type) {
+        continue;
+      } else if ((declaration_type == ^^declaration const&) && (candidate_type == add_lvalue_reference(add_const(self_t)))) {
+        continue;
+      } else if ((declaration_type == ^^declaration&) && (candidate_type == add_lvalue_reference(self_t))) {
+         continue;
+      } else {
+        return false;
+      }
+    }
+    return true;
+}
+
+consteval std::meta::info get_implementation_member(std::meta::info in, std::meta::info self_t, std::meta::info declaration_member) {
+    constexpr auto ctx = std::meta::access_context::current();
+    for(auto candidate : members_of(in, ctx)) {
+      if (meta::function_name_of(candidate) == meta::function_name_of(declaration_member)) {
+        auto m_params = parameters_of(candidate) 
+            | std::views::drop(is_static_member(candidate) ? 1 : 0); 
+        auto d_params = parameters_of(declaration_member) 
+            | std::views::drop(is_static_member(declaration_member) ? 1 : 0);
+        if (same_signature(self_t, m_params, d_params)) {
+            return candidate;
+        }
+      }
+    }
+    return {};
 }
 
 }  // namespace anyxx26::meta
