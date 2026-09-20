ac0ecbe50150c03f444ffd342dd9ef03a74484ce
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-20 09:10:30 +0200

    Switch loop and condition from constexpr to runtime

--- examples/anyxx26/dyn/facade.hpp
+++ examples/anyxx26/dyn/facade.hpp
@@ -81,8 +81,8 @@ consteval void collect_overload_set_for_name(auto id, std::vector<std::meta::inf
         collect_overload_set_for_name<DynBase, base>(id, overload_set);
     }
     constexpr auto ctx = std::meta::access_context::current();
-    template for(constexpr auto m : define_static_array(members_of(TraitDeclaration, ctx))) {
-        if constexpr(is_function(m) && is_user_declared(m)) {
+    for(auto m : define_static_array(members_of(TraitDeclaration, ctx))) {
+        if (is_function(m) && is_user_declared(m)) {
             if(meta::function_name_of(m) == id) {
                 overload_set.push_back(make_dyn_facade_call<DynBase>(m));
             }
