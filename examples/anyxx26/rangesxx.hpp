e7a702a0c51856d19571f800db05c60191bc1c8c
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-20 20:52:32 +0200

    Refactor iterator hierarchy; add sentinel base struct

--- examples/anyxx26/rangesxx.hpp
+++ examples/anyxx26/rangesxx.hpp
@@ -4,14 +4,20 @@
 
 namespace anyxx26::rangesxx {
 
-template <typename Self, typename Trait, typename Value>
-struct input_iterator : save_copyable<Self, Trait> {
+template <typename Self, typename Trait>
+struct sentinel : save_copyable<Self, Trait> {
     using default_proxy_t = anyxx::val<std::true_type>;
+
+};
+
+template <typename Self, typename Trait, typename Value>
+struct input_iterator : sentinel<Self, Trait> {
+
+    bool operator==(Self const&) const;
+    bool operator!=(Self const&) const;
 
     Value& operator*();
     Self& operator++();
-    bool operator==(Self const&) const;
-    bool operator!=(Self const&) const;
 
 	struct typenames {
 		using value_type = Value;
