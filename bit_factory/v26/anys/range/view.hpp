5428e71562130b08ff1c218db028084fc250de41
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-23 09:34:12 +0200

    Refactor common code from view and sized_view into base_view

--- bit_factory/v26/anys/range/view.hpp
+++ bit_factory/v26/anys/range/view.hpp
@@ -1,26 +1,14 @@
 #pragma once
 
-#include <bit_factory/v26/any.hpp>
-#include <bit_factory/v26/anys/range/iterators.hpp>
-#include <bit_factory/v26/anys/range/iterator_categories.hpp>
+#include <bit_factory/v26/anys/range/view_base.hpp>
 #include <bit_factory/v26/anys/range/sentinels.hpp>
 
 namespace anyxx26::rangesxx {
 
 template <is_trait Trait, typename Self, typename Category, typename Value>
-struct view : save_copyable<Trait, Self> {
-    using default_proxy_t = anyxx::val<std::true_type>;
-    template <typename Any>
-    using trait_facade_decorator = std::ranges::view_interface<Any>;
-
-    static any<Category::template iterator, Value> begin(Self& self) {
-        return std::ranges::begin(self);
-    }
+struct view : view_base<Trait, Self, Category, Value> {
     static any<sentinel, Value> end(Self& self) {
         return make_end_sentinel_for(self);
-    }
-    static any<Category::template iterator, Value const> begin(Self const& self) {
-        return std::ranges::begin(self);
     }
     static any<sentinel, Value const> end(Self const& self) {
         return make_end_sentinel_for(self);
