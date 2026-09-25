ba7de09443ede9baf96b45ea52e0ecd216ebc1c0
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2026-09-25 15:36:38 +0200

    self_cast

--- bit_factory/v26/trait_as.hpp
+++ bit_factory/v26/trait_as.hpp
@@ -13,20 +13,23 @@
 
 namespace anyxx26 {
 
+template <typename V, typename Self>
+decltype(auto) self_cast(Self* self){
+    return static_cast<V*>(static_cast<std::conditional_t<std::is_const_v<Self>, const void, void>*>(self));
+}
+
 template <typename V, std::meta::info Target>
-struct const_trait_facade_model_map_call {
+struct const_trait_model_map_call {
   template <typename... Args>
   decltype(auto) operator()(Args&&... args) const {
-    auto* pvalue = reinterpret_cast<V*>(this);
-    return [:Target:](*pvalue, std::forward<Args>(args)...);
+        return[:Target:](*self_cast<V>(this), std::forward<Args>(args)...);
   }
 };
 template <typename V, std::meta::info Target>
-struct mutable_trait_facade_model_map_call {
+struct mutable_trait_model_map_call {
     template <typename... Args>
     decltype(auto) operator()(Args&&... args) {
-      auto pvalue = reinterpret_cast<V*>(this);
-      return [:Target:](*pvalue, std::forward<Args>(args)...);
+        return[:Target:](*self_cast<V>(this), std::forward<Args>(args)...);
     }
 };
 
@@ -34,16 +37,14 @@ template <typename V, std::meta::info Target>
 struct const_trait_member_call {
     template <typename... Args>
     decltype(auto) operator()(Args&&... args) const {
-        auto* pvalue = reinterpret_cast<V*>(this);
-        return pvalue->[:Target:](std::forward<Args>(args)...);
+        return self_cast<V>(this)->[:Target:](std::forward<Args>(args)...);
     }
 };
 template <typename V, std::meta::info Target>
 struct mutable_trait_member_call {
     template <typename... Args>
     decltype(auto) operator()(Args&&... args) {
-        auto pvalue = reinterpret_cast<V*>(this);
-        return pvalue->[:Target:](std::forward<Args>(args)...);
+        return self_cast<V>(this)->[:Target:](std::forward<Args>(args)...);
     }
 };
 
@@ -67,9 +68,9 @@ template <typename V, template <typename, typename, typename...> typename Trait,
 consteval std::meta::info make_static_facade_call(interface_spec_with_target const& spec){
     if (!is_defaulted_function_spec(spec.target)) {
         if(is_const_function(spec.target)) {
-            return substitute(^^const_trait_facade_model_map_call, {^^V const, reflect_constant(spec.target)});
+            return substitute(^^const_trait_model_map_call, {^^V const, reflect_constant(spec.target)});
         } else {
-            return substitute(^^mutable_trait_facade_model_map_call, {^^V, reflect_constant(spec.target)});
+            return substitute(^^mutable_trait_model_map_call, {^^V, reflect_constant(spec.target)});
         }
     } else {
         if (is_const_function(spec.member)) {
